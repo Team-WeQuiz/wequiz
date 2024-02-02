7edf91a4138464c4596a6571dfe7682e574cac8a
koomchang <leahpar0401@gmail.com>
2024-02-02 13:16:29 +0900

    [feat/#15] :wrench: Config: initialize Spring boot

---
+++ chatty-be/src/test/java/com/chatty/chatty/ChattyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chatty.chatty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChattyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
