53c9caa2f261a4b9aa0f73dd39babda3073b355b
ryanbae94 <ryanbae94@gmail.com>
2024-02-02 22:24:20 +0900

    [feat/#21] :art: Style: page transition animation

--- chatty-fe/src/app/(chatbotSelection)/_layout.tsx
+++ chatty-fe/src/app/(chatbotSelection)/_layout.tsx
@@ -3,8 +3,6 @@ import { useRouter } from 'next/navigation';
 import * as styles from './layout.css';
 import Image from 'next/image';
 import ProgressBar from './_component/ProgressBar';
-import { Transition } from 'react-transition-group';
-import { useEffect, useState } from 'react';
 
 type SelectionLayoutProps = {
 	children: React.ReactNode;
@@ -21,54 +19,18 @@ export default function SelectionLayout({
 	const handleBackBtnClick = () => {
 		navigate.back();
 	};
-	const duration = 200;
-	const defaultStyle = {
-		transition: `opacity ${duration}ms ease-in-out`,
-		opacity: 0,
-	};
-	const transitionStyles = {
-		entering: { opacity: 1 },
-		entered: { opacity: 1 },
-		exiting: { opacity: 0 },
-		exited: { opacity: 0 },
-		unmounted: { opacity: 0 },
-	};
-	const [inProp, setInprop] = useState(false);
-
-	useEffect(() => {
-		setInprop(true);
-	}, []);
 
 	return (
-		<Transition timeout={duration} in={inProp}>
-			{(state) => (
-				<div
-					style={{
-						...defaultStyle,
-						...transitionStyles[state],
-					}}
-				>
-					<div className={styles.container}>
-						<div className={styles.headerWrapper}>
-							<div
-								className={styles.backBtnWrapper}
-								onClick={handleBackBtnClick}
-							>
-								<Image
-									src='/images/back.svg'
-									alt='back'
-									width={52}
-									height={52}
-								/>
-							</div>
-							<h1 className={styles.title}>{title}</h1>
-							<div className={styles.backBtnWrapper} />
-						</div>
-						<ProgressBar progress={progress} />
-						{children}
-					</div>
+		<div className={styles.container}>
+			<div className={styles.headerWrapper}>
+				<div className={styles.backBtnWrapper} onClick={handleBackBtnClick}>
+					<Image src='/images/back.svg' alt='back' width={52} height={52} />
 				</div>
-			)}
-		</Transition>
+				<h1 className={styles.title}>{title}</h1>
+				<div className={styles.backBtnWrapper} />
+			</div>
+			<ProgressBar progress={progress} />
+			{children}
+		</div>
 	);
 }
