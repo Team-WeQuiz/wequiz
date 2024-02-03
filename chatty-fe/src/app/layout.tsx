d38908989b19ed0fc48b1867506ab82804a768c2
JunHyung Bae <ryanbae94@gmail.com>
2024-02-03 20:07:24 +0900

    페이지 전환 애니메이션 (#26)

--- chatty-fe/src/app/layout.tsx
+++ chatty-fe/src/app/layout.tsx
@@ -1,8 +1,12 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
+import localFont from 'next/font/local';
 import './globals.css';
 
 const inter = Inter({ subsets: ['latin'] });
+const pretendard = localFont({
+	src: '../../public/fonts/PretendardVariable.ttf',
+});
 
 export const metadata: Metadata = {
 	title: 'Create Next App',
@@ -16,7 +20,7 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang='en'>
-			<body className={inter.className}>{children}</body>
+			<body className={pretendard.className}>{children}</body>
 		</html>
 	);
 }
