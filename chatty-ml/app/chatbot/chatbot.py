fce6dd6da3de828ac1029001258342ff9abcf5a0
noooey <20203065@kookmin.ac.kr>
2024-02-04 02:09:43 +0900

    [feat/#31] ♻️ Refact : 폴더 재구조화

---
+++ chatty-ml/app/chatbot/chatbot.py
@@ -0,0 +1,27 @@
+import gradio as gr
+from model.chain import Chain
+
+class Chatbot():
+    def __init__(self):
+        self.chain = chain = Chain()
+
+    # gardio에 삽입할 함수
+    def answer(self, message, history):
+        return self.chain.inference(message, history)
+    # 그라디오 챗봇 클라이언트
+    def set_client(self):
+        client = gr.ChatInterface(
+                    self.answer,
+                    chatbot=gr.Chatbot(height=600),
+                    textbox=gr.Textbox(placeholder="Chatty에게 질문해주세요", container=False, scale=7),
+                    title="Chatty",
+                    description="맞춤형 AI 과외선생님 Chatty에게 질문하세요!",
+                    retry_btn=None,
+                    undo_btn=None,
+                    clear_btn=None,
+                )
+        return client
+    
+    def run(self):
+        client = self.set_client()
+        client.launch()
