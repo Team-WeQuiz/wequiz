b4970b039cc8d34a53f08d08d6eb5a6f5911bdd6
koomchang <leahpar0401@gmail.com>
2024-02-04 12:37:47 +0900

    [feat/#35] :sparkles: Add: BaseEntity 구현

---
+++ chatty-be/src/main/java/com/chatty/chatty/common/domain/BaseEntity.java
@@ -0,0 +1,28 @@
+package com.chatty.chatty.common.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public abstract class BaseEntity {
+
+    @Column(updatable = false, nullable = false)
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @Column(nullable = false)
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+}
