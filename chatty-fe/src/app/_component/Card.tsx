bea2bdcff6a2dc1568b426aaaafd8730c4e66853
ryanbae94 <ryanbae94@gmail.com>
2024-02-05 02:58:07 +0900

    [feat/#37] :recycle: Refactor: main page

--- chatty-fe/src/app/_component/Card.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import * as styles from './styles/card.css';
-
-type CardProps = {};
-
-export default function Card() {
-	return <div className={styles.container} />;
-}
