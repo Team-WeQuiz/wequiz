a34f2f8cbe9cf03bde81e6afbb4cf6a7dd608b6b
ryanbae94 <ryanbae94@gmail.com>
2024-02-05 14:39:44 +0900

    [feat/#37] :sparkles: Add: initialize msw server

--- chatty-fe/src/app/layout.tsx
+++ chatty-fe/src/app/layout.tsx
@@ -1,27 +1,27 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import localFont from "next/font/local";
-import "./globals.css";
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import localFont from 'next/font/local';
+import './globals.css';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 const pretendard = localFont({
-  src: "../../public/fonts/PretendardVariable.ttf",
-  variable: "--main-font",
+	src: '../../public/fonts/PretendardVariable.ttf',
+	variable: '--main-font',
 });
 
 export const metadata: Metadata = {
-  title: "Chatty",
-  description: "Chatty is an AI chatbot service that helps you study.",
+	title: 'Chatty',
+	description: 'Chatty is an AI chatbot service that helps you study.',
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body className={pretendard.variable}>{children}</body>
-    </html>
-  );
+	return (
+		<html lang='en'>
+			<body className={pretendard.variable}>{children}</body>
+		</html>
+	);
 }
--- chatty-fe/src/app/page.tsx
+++ chatty-fe/src/app/page.tsx
@@ -2,12 +2,15 @@
 
 import { useEffect } from 'react';
 import { useRouter } from 'next/navigation';
+import initMockAPI from './_mocks';
+if (process.env.NODE_ENV === 'development') {
+	initMockAPI();
+}
 
 export default function Home() {
 	const navigate = useRouter();
 	useEffect(() => {
 		navigate.push('/main');
 	}, []);
-
 	return <div></div>;
 }
