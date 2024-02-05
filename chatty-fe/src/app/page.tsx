098e3f611f08905ec25faedb705c3e44924dd266
ryanbae94 <ryanbae94@gmail.com>
2024-02-06 04:54:12 +0900

    [feat/#37] :sparkles: Add: variable to enable msw

--- chatty-fe/src/app/page.tsx
+++ chatty-fe/src/app/page.tsx
@@ -3,7 +3,11 @@
 import { useEffect } from 'react';
 import { useRouter } from 'next/navigation';
 import initMockAPI from './_mocks';
-if (process.env.NODE_ENV === 'development') {
+
+if (
+	process.env.NODE_ENV === 'development' &&
+	process.env.NEXT_PUBLIC_API_MOCKING === 'enable'
+) {
 	initMockAPI();
 }
 
