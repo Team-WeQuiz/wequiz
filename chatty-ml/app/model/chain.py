e78e540cd9371a68148790146ec769d94efd0043
noooey <20203065@kookmin.ac.kr>
2024-02-09 23:21:47 +0900

    [feat/#33] ✨ Feat: 질문과 유사한 문서 기반 답변 파이프라인 구현
    Related to #33

--- chatty-ml/app/model/chain.py
+++ chatty-ml/app/model/chain.py
@@ -7,20 +7,28 @@ from model.prompt import SYSTEM_PROMPT
 from langchain_openai import ChatOpenAI
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnablePassthrough
+from langchain_community.vectorstores import FAISS
+from model.embedding import Embedding
+from operator import itemgetter
 
 # LLM 체인 클래스
 class Chain():
     def __init__(self):
-        self.docs = ''
+        self.vectorstore= FAISS.load_local('./data/faiss_index', embeddings=Embedding('query').model)
+        self.retriever = self.vectorstore.as_retriever()
         self.llm = ChatOpenAI()
-        self.prompt = ChatPromptTemplate.from_messages([
-            ("system", SYSTEM_PROMPT),
-            ("user", "{input}")
-        ])
+        self.prompt = ChatPromptTemplate.from_template(SYSTEM_PROMPT)
         self.output_parser = StrOutputParser()
-        self.chain = self.prompt | self.llm | self.output_parser   # Chain 구성
+        self.chain = (
+            {"context": self.retriever, "question": RunnablePassthrough()}
+            | self.prompt
+            | self.llm
+            | self.output_parser 
+          )  # Chain 구성
     
     # LLM inference 함수
     def inference(self, message, history):
-        return self.chain.invoke({"input": message})
+        print(self.retriever.invoke(message))
+        return self.chain.invoke(message)
 
