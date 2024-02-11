7e61a1331f638d7e1e0aa22ec7e5061cc9e2c7d8
ryanbae94 <ryanbae94@gmail.com>
2024-02-11 21:52:56 +0900

    [feat/#42] :sparkles: Add: remove deps

--- chatty-fe/src/app/api/s3-upload/route.ts
+++ chatty-fe/src/app/api/s3-upload/route.ts
@@ -2,7 +2,6 @@ import {
 	S3Client,
 	PutObjectCommand,
 	GetObjectCommand,
-	ListObjectsCommand,
 } from '@aws-sdk/client-s3';
 import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
 import { NextRequest, NextResponse } from 'next/server';
