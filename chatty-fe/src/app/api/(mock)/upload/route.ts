bc3b6715e4cb3a2faf63e7975fd44a503be61f4b
ryanbae94 <ryanbae94@gmail.com>
2024-02-11 21:53:09 +0900

    [feat/#42] :sparkles: Add: update serverside url

--- chatty-fe/src/app/api/(mock)/upload/route.ts
+++ chatty-fe/src/app/api/(mock)/upload/route.ts
@@ -1,10 +1,11 @@
 import { NextRequest, NextResponse } from 'next/server';
 
+const url: string[] = [];
 export async function POST(request: NextRequest) {
-	const url: string[] = [];
 	try {
-		const req = request.json();
-		return NextResponse.json({ status: 200, req });
+		const req = await request.json();
+		url.push(req.url);
+		return NextResponse.json({ status: 201, req, url });
 	} catch (error) {
 		return NextResponse.json({ error });
 	}
