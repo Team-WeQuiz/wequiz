358f827b5478e3d2d7cb2ec7dcb8e262cc93c26f
ryanbae94 <ryanbae94@gmail.com>
2024-02-11 21:44:50 +0900

    [feat/#42] :sparkles: Add: migrate mock

--- chatty-fe/src/app/(chatbotSelection)/upload-file/_component/UploadButton.tsx
+++ chatty-fe/src/app/(chatbotSelection)/upload-file/_component/UploadButton.tsx
@@ -1,14 +1,35 @@
-import React from 'react';
+'use client';
+import React, { useRef } from 'react';
 import * as styles from './styles/uploadButton.css';
 
 type UploadButtonProps = {
-	onClick: () => void;
+	onFileSelected: (file: File) => void;
 };
 
-export default function UploadButton({ onClick }: UploadButtonProps) {
+export default function UploadButton({ onFileSelected }: UploadButtonProps) {
+	const fileInputRef = useRef<HTMLInputElement>(null);
+
+	const handleFileSelect = (e: React.ChangeEvent<HTMLInputElement>) => {
+		if (e.target.files && e.target.files.length > 0) {
+			onFileSelected(e.target.files[0]);
+		}
+	};
+
+	const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+		e.preventDefault();
+		fileInputRef.current?.click();
+	};
 	return (
-		<button className={styles.button} onClick={onClick}>
-			파일 선택
-		</button>
+		<>
+			<input
+				type='file'
+				ref={fileInputRef}
+				onChange={handleFileSelect}
+				style={{ display: 'none' }}
+			/>
+			<button className={styles.button} onClick={handleClick}>
+				파일 선택
+			</button>
+		</>
 	);
 }
