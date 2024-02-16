f13e89121e17e83f824fa67f1e41bb9169a9b610
koomchang <leahpar0401@gmail.com>
2024-02-16 23:30:23 +0900

    [feat/#52] :recycle: Refactor: User 엔티티에서 username 사용 변경

--- chatty-be/src/main/java/com/chatty/chatty/user/repository/UserRepository.java
+++ chatty-be/src/main/java/com/chatty/chatty/user/repository/UserRepository.java
@@ -1,2 +1,10 @@
-package com.chatty.chatty.user.repository;public interface UserRepository {
+package com.chatty.chatty.user.repository;
+
+import com.chatty.chatty.user.entity.User;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByUsername(String username);
 }
