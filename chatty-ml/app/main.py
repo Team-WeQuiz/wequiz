ce490f59fe133ff7b8ff2cef24bb766ae66592da
noooey <20203065@kookmin.ac.kr>
2024-02-18 17:56:37 +0900

    [feat/#45] ✏️ Typo: 필요없는 코드 제거

--- chatty-ml/app/main.py
+++ chatty-ml/app/main.py
@@ -1,15 +1,6 @@
-import os
-import yaml
 from client.chatbot import Chatbot
 
 if __name__ == "__main__":
-    # Get Config
-    with open('config.yaml', 'r') as file:
-        config = yaml.safe_load(file)
-        
-    # Open AI API Key
-    os.environ["OPENAI_API_KEY"] = config.get("OPENAI_API_KEY")
-    
     # 챗봇 실행
     chatbot = Chatbot()
     chatbot.run()
