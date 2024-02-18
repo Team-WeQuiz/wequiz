bf389b07aed2922a1d1a611dc85d951172da842f
noooey <20203065@kookmin.ac.kr>
2024-02-18 17:59:47 +0900

    [feat/#45] ♻️ Refact: 폴더 경로 이동

--- chatty-ml/app/app.py
+++ chatty-ml/app/app.py
@@ -1,15 +1,3 @@
-import sys
-import os
-
-# Get the parent directory of the current script (app directory)
-current_dir = os.path.dirname(os.path.realpath(__file__))
-parent_dir = os.path.abspath(os.path.join(current_dir, os.pardir))
-
-# Add the parent directory to sys.path
-sys.path.append(parent_dir)
-
-###################################################################################
-
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel 
 from model.chain import Chain
