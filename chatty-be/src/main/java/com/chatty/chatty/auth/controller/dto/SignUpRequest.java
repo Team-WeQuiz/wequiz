744d05523bd04df903a97e281dbabdb3219203d2
cherry031 <20203093@kookmin.ac.kr>
2024-02-28 01:46:44 +0900

    [feat/#66] :recycle: Refactor: 회원가입 시 닉네임 받는 부분 삭제

--- chatty-be/src/main/java/com/chatty/chatty/auth/controller/dto/SignUpRequest.java
+++ chatty-be/src/main/java/com/chatty/chatty/auth/controller/dto/SignUpRequest.java
@@ -2,8 +2,7 @@ package com.chatty.chatty.auth.controller.dto;
 
 public record SignUpRequest(
         String email,
-        String password,
-        String nickname
+        String password
 ) {
 
 }
