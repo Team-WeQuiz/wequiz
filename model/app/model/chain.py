f3062609fe9d6c8fca82e63e7e01c06b2d24a0ab
noooey <20203065@kookmin.ac.kr>
2024-03-03 00:38:41 +0900

    [feat/#62] ✨ Feat: 문제 형식별 프롬프트 생성
    Related to #62

--- model/app/model/chain.py
+++ model/app/model/chain.py
@@ -3,8 +3,9 @@ TODO
 - 챗봇 id에 따라 docs db의 path를 가져와야함
 """
 
-from model.prompt import SYSTEM_PROMPT 
-from langchain_openai import ChatOpenAI
+from model.prompt import CHOICE_PROB_PROMPT, LONG_PROB_PROMPT, SHORT_PROB_PROMPT, KEY_EXCT_PROMPT
+from langchain_openai import OpenAI
+from langchain.prompts import PromptTemplate
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.runnables import RunnablePassthrough
@@ -14,15 +15,19 @@ from utils.logger import log
 
 # LLM 체인 클래스
 class Chain():
-    def __init__(self):
-        self.vectorstore= FAISS.load_local('./data/faiss_index3', embeddings=Embedding('query').model)
+    def __init__(self, type):
+        self.vectorstore= FAISS.load_local('./data/faiss_index2', embeddings=Embedding('query').model)
         self.retriever = self.vectorstore.as_retriever()
-        self.llm = ChatOpenAI()
-        self.prompt = ChatPromptTemplate.from_template(SYSTEM_PROMPT)
+        self.llm = OpenAI()
+        self.prompt = {
+            "choice": PromptTemplate.from_template(CHOICE_PROB_PROMPT),
+            "long": PromptTemplate.from_template(LONG_PROB_PROMPT),
+            "short": PromptTemplate.from_template(SHORT_PROB_PROMPT)
+        }
         self.output_parser = StrOutputParser()
         self.chain = (
-            {"context": self.retriever, "question": RunnablePassthrough()}
-            | self.prompt
+            {"context": self.retriever, "message": RunnablePassthrough()}
+            | self.prompt[type]
             | self.llm
             | self.output_parser 
           )  # Chain 구성
