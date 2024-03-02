7a884a6e89f4ec0755579336aac767f96810a66f
noooey <20203065@kookmin.ac.kr>
2024-03-03 03:13:00 +0900

    [feat/#62] ✨ Feat: JSON output parser

--- model/app/model/chain.py
+++ model/app/model/chain.py
@@ -1,38 +1,37 @@
-"""
-TODO
-- 챗봇 id에 따라 docs db의 path를 가져와야함
-"""
-
-from model.prompt import CHOICE_PROB_PROMPT, LONG_PROB_PROMPT, SHORT_PROB_PROMPT, KEY_EXCT_PROMPT
+from re import template
+from model.prompt import SYSTEM_PROMPT
 from langchain_openai import OpenAI
 from langchain.prompts import PromptTemplate
-from langchain_core.prompts import ChatPromptTemplate
-from langchain_core.output_parsers import StrOutputParser
-from langchain_core.runnables import RunnablePassthrough
+from operator import itemgetter
+from typing import List
+from langchain_core.pydantic_v1 import BaseModel, Field
+from langchain_core.output_parsers import JsonOutputParser
 from langchain_community.vectorstores import FAISS
 from model.embedding import Embedding
 from utils.logger import log
 
+# 문제 형식 정의
+class Question(BaseModel):
+    question: str = Field(description="question about keywords")
+    choices: List[str] = Field(description="Five options for the problem. Among these, there is only one correct answer.")
+    answer: str = Field(description="The correct option among the given choices")
+
 # LLM 체인 클래스
 class Chain():
-    def __init__(self, db_path, type):
+    def __init__(self, db_path):
         self.vectorstore= FAISS.load_local(db_path, embeddings=Embedding('query').model)
         self.retriever = self.vectorstore.as_retriever()
         self.llm = OpenAI()
-        self.prompt = {
-            "choice": PromptTemplate.from_template(CHOICE_PROB_PROMPT),
-            "long": PromptTemplate.from_template(LONG_PROB_PROMPT),
-            "short": PromptTemplate.from_template(SHORT_PROB_PROMPT)
-        }
-        self.output_parser = StrOutputParser()
-        self.chain = (
-            {"context": self.retriever, "message": RunnablePassthrough()}
-            | self.prompt[type]
-            | self.llm
-            | self.output_parser 
-          )  # Chain 구성
+        self.parser = JsonOutputParser(pydantic_object=Question)
+        self.prompt = PromptTemplate(
+            template=SYSTEM_PROMPT,
+            input_variables=["message", "type"],
+            partial_variables={"context": self.retriever, "format_instructions": self.parser.get_format_instructions()}
+        )
+        self.chain = self.prompt | self.llm | self.parser
+        log('info', 'Setting Chain successfully.')
     
     # LLM inference 함수
-    def inference(self, message, history=None):
-        return self.chain.invoke(message)
+    def inference(self, message, type):
+        return self.chain.invoke({"message": message, "type": type})
 
--- model/app/model/prompt.py
+++ model/app/model/prompt.py
@@ -2,21 +2,12 @@ SYSTEM_PROMPT = """
 당신은 학습용 문제를 생성해주는 친절한 인공지능 모델입니다.
 
 사용자는 키워드를 제시합니다.
-키워드: {question}
+키워드: {message}
 
-아래 문서를 기반으로 키워드에 대한 '객관식' 문제를 만들어주세요.
-사용자는 당신의 문제에 대한 답변으로 보기 중 하나를 선택합니다.
-반드시 문제 형식에 맞춰서 답변해주세요. 
-
+아래 문서를 기반으로 키워드에 대한 객관식 문제를 만들어주세요.
 문서: {context}
 
-문제 형식:
-[문제] question_context
-[보기]
-(1) answer_option_1
-(2) answer_option_2
-(3) answer_option_3
-(4) answer_option_4
+{format_instructions}
 """
 
 KEY_EXCT_PROMPT = """
@@ -25,67 +16,4 @@ KEY_EXCT_PROMPT = """
 
 아래 문서에서 주제에 해당하는 내용 중 메인 키워드와 그에 대한 내용을 python dictionary 형태로 답변하세요.
 문서: {context}
-"""
-
-CHOICE_PROB_PROMPT = """
-당신은 학습용 문제를 생성해주는 친절한 인공지능 모델입니다.
-
-사용자는 키워드를 제시합니다.
-키워드: {message}
-
-아래 문서를 기반으로 사용자의 키워드에 대한 '객관식' 문제를 만들어주세요.
-사용자는 당신의 문제에 대한 답변으로 보기 중 하나를 선택합니다.
-반드시 문제 형식에 맞춰서 답변해주세요. 
-
-문서: {context}
-
-문제 형식:
-[문제]
-question_context
-[보기]
-(1) answer_option_1
-(2) answer_option_2
-(3) answer_option_3
-(4) answer_option_4
-(5) answer_option_5
-[정답]
-answer
-"""
-
-LONG_PROB_PROMPT = """
-당신은 학습용 문제를 생성해주는 친절한 인공지능 모델입니다.
-
-사용자는 키워드를 제시합니다.
-키워드: {message}
-
-아래 문서를 기반으로 키워드에 대한 '주관식' 문제를 만들어주세요.
-사용자는 당신의 문제에 대한 답변으로 '문장'을 제시합니다.
-반드시 문제 형식에 맞춰서 답변해주세요. 
-
-문서: {context}
-
-문제 형식:
-[문제]
-question_context
-[정답]
-answer
-"""
-
-SHORT_PROB_PROMPT = """
-당신은 학습용 문제를 생성해주는 친절한 인공지능 모델입니다.
-
-사용자는 키워드를 제시합니다.
-키워드: {message}
-
-아래 문서를 기반으로 키워드에 대한 '단답형' 문제를 만들어주세요.
-사용자는 당신의 문제에 대한 답변으로 하나의 '단어'를 제시합니다.
-반드시 문제 형식에 맞춰서 답변해주세요. 
-
-문서: {context}
-
-문제 형식:
-[문제]
-question_context
-[정답]
-answer
 """
