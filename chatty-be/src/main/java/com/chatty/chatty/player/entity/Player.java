01ed91cbfaa4e73639d1e7e3d552bc65ed489fbf
cherry031 <20203093@kookmin.ac.kr>
2024-03-06 00:22:14 +0900

    [feat/#76] :recycle: refactor: player, quizroom 컬럼 수정

--- chatty-be/src/main/java/com/chatty/chatty/player/entity/Player.java
+++ chatty-be/src/main/java/com/chatty/chatty/player/entity/Player.java
@@ -1,21 +1,20 @@
 package com.chatty.chatty.player.entity;
 
 import com.chatty.chatty.common.entity.BaseEntity;
-import com.chatty.chatty.quizroom.entity.Quizroom;
+import com.chatty.chatty.quizroom.entity.QuizRoom;
 import com.chatty.chatty.user.entity.User;
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
-import jakarta.persistence.EmbeddedId;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.MapsId;
-import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.DynamicInsert;
@@ -28,27 +27,25 @@ import org.hibernate.annotations.DynamicUpdate;
 @Builder
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EqualsAndHashCode(of = "id", callSuper = false)
 @Table(name = "`players`")
 public class Player extends BaseEntity {
 
-    @EmbeddedId
-    private PlayerId id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "player_id")
+    private Long id;
 
-    @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @ManyToOne
     @JoinColumn(name = "user_id")
-    @MapsId("user_id")
     private User user;
 
-    @ManyToOne(cascade = CascadeType.PERSIST, fetch = FetchType.LAZY)
+    @ManyToOne
     @JoinColumn(name = "room_id")
-    @MapsId("room_id")
-    private Quizroom quizroom;
+    private QuizRoom quizRoom;
 
     @Column(unique = true, length = 10, nullable = false)
     private String nickname;
-
-    @Column(columnDefinition = "INTEGER NOT NULL DEFAULT 0")
-    private Integer progress;
 
     @Column(columnDefinition = "INTEGER NOT NULL DEFAULT -1")
     private Integer timeTaken;
