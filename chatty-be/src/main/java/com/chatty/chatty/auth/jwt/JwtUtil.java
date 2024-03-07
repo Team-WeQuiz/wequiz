b39f8aa4b8912c9b36913d1e4e952c5e4f8049e1
koomchang <leahpar0401@gmail.com>
2024-03-07 12:49:15 +0900

    [feat/#79] :sparkles: Feat: JWT 관련 예외 처리 추가

--- chatty-be/src/main/java/com/chatty/chatty/auth/jwt/JwtUtil.java
+++ chatty-be/src/main/java/com/chatty/chatty/auth/jwt/JwtUtil.java
@@ -1,18 +1,33 @@
 package com.chatty.chatty.auth.jwt;
 
+import static com.chatty.chatty.auth.exception.AuthExceptionType.EXPIRED_TOKEN;
+import static com.chatty.chatty.auth.exception.AuthExceptionType.INVALID_SIGNATURE;
+import static com.chatty.chatty.auth.exception.AuthExceptionType.INVALID_TOKEN;
+import static com.chatty.chatty.auth.exception.AuthExceptionType.MALFORMED_TOKEN;
+import static com.chatty.chatty.auth.exception.AuthExceptionType.SIGNATURE_NOT_FOUND;
+import static com.chatty.chatty.auth.exception.AuthExceptionType.UNSUPPORTED_TOKEN;
+
+import com.chatty.chatty.auth.exception.AuthException;
+import com.chatty.chatty.auth.exception.AuthExceptionType;
 import com.chatty.chatty.user.entity.User;
 import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.Jwts.SIG;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.UnsupportedJwtException;
+import io.jsonwebtoken.security.SignatureException;
 import java.nio.charset.StandardCharsets;
 import java.util.Date;
 import javax.crypto.SecretKey;
 import javax.crypto.spec.SecretKeySpec;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
+@Slf4j
 public class JwtUtil {
 
     private final SecretKey secretKey;
@@ -29,8 +44,9 @@ public class JwtUtil {
     }
 
     public String createAccessToken(User user) {
+        log.info("user.getId(): {}", user.getId());
         return Jwts.builder()
-                .claim("email", user.getEmail())
+                .claim("id", user.getId())
                 .issuedAt(new Date(System.currentTimeMillis()))
                 .expiration(new Date(System.currentTimeMillis() + accessExpiration))
                 .signWith(secretKey)
@@ -39,18 +55,43 @@ public class JwtUtil {
 
     public String createRefreshToken(User user) {
         return Jwts.builder()
-                .claim("email", user.getEmail())
+                .claim("id", user.getId())
                 .expiration(new Date(System.currentTimeMillis() + refreshExpiration))
                 .signWith(secretKey)
                 .compact();
     }
 
     public boolean isValidRefreshToken(String refreshToken) {
+        if (isTokenExpired(refreshToken)) {
+            return false;
+        }
         try {
             getClaimFromToken(refreshToken);
             return true;
         } catch (NullPointerException | JwtException e) {
             return false;
+        }
+    }
+
+    private boolean isTokenExpired(String token) {
+        return getClaimFromToken(token).getExpiration().before(new Date());
+    }
+
+    public Long extract(String token) {
+        try {
+            return getClaimFromToken(token).get("id", Long.class);
+        } catch (SecurityException e) {
+            throw new AuthException(SIGNATURE_NOT_FOUND);
+        } catch (MalformedJwtException e) {
+            throw new AuthException(MALFORMED_TOKEN);
+        } catch (ExpiredJwtException e) {
+            throw new AuthException(EXPIRED_TOKEN);
+        } catch (UnsupportedJwtException e) {
+            throw new AuthException(UNSUPPORTED_TOKEN);
+        } catch (IllegalArgumentException e) {
+            throw new AuthException(INVALID_TOKEN);
+        } catch (SignatureException e) {
+            throw new AuthException(INVALID_SIGNATURE);
         }
     }
 
@@ -61,5 +102,4 @@ public class JwtUtil {
                 .parseSignedClaims(token)
                 .getPayload();
     }
-
 }
