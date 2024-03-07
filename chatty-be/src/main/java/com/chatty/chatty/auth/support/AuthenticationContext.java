d3cbc565c49bcbcf6a546b12a837659b173e0f38
koomchang <leahpar0401@gmail.com>
2024-03-07 12:48:16 +0900

    [feat/#79] :sparkles: Feat: Authentication 정보를 담을 Context와 관련 util 클래스 구현

--- chatty-be/src/main/java/com/chatty/chatty/auth/support/AuthenticationContext.java
+++ chatty-be/src/main/java/com/chatty/chatty/auth/support/AuthenticationContext.java
@@ -1,2 +1,29 @@
-package com.chatty.chatty.auth.support;public class AuthenticationContext {
+package com.chatty.chatty.auth.support;
+
+import static com.chatty.chatty.auth.exception.AuthExceptionType.UNAUTHORIZED;
+
+import com.chatty.chatty.auth.exception.AuthException;
+import java.util.Objects;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.annotation.RequestScope;
+
+@RequestScope
+@Component
+@Slf4j
+public class AuthenticationContext {
+
+    private Long userId;
+
+    public void setAuthentication(Long userId) {
+        this.userId = userId;
+    }
+
+    public Long getPrincipal() {
+        log.info("userId: {}", userId);
+        if (Objects.isNull(userId)) {
+            throw new AuthException(UNAUTHORIZED);
+        }
+        return userId;
+    }
 }
--- chatty-be/src/main/java/com/chatty/chatty/auth/support/AuthenticationExtractor.java
+++ chatty-be/src/main/java/com/chatty/chatty/auth/support/AuthenticationExtractor.java
@@ -1,2 +1,27 @@
-package com.chatty.chatty.auth.support;public class AuthenticationExtractor {
+package com.chatty.chatty.auth.support;
+
+import jakarta.servlet.http.HttpServletRequest;
+import java.util.Optional;
+import org.springframework.util.StringUtils;
+
+public class AuthenticationExtractor {
+
+    private static final String AUTHORIZATION_HEADER = "Authorization";
+    private static final String BEARER = "Bearer";
+
+    public static Optional<String> extract(HttpServletRequest request) {
+        String header = request.getHeader(AUTHORIZATION_HEADER);
+        if (!StringUtils.hasText(header)) {
+            return Optional.empty();
+        }
+        return checkMatch(header.split(" "));
+    }
+
+    private static Optional<String> checkMatch(String[] parts) {
+        if (parts.length == 2 && parts[0].equals(BEARER)) {
+            return Optional.ofNullable(parts[1]);
+        }
+        return Optional.empty();
+    }
+
 }
