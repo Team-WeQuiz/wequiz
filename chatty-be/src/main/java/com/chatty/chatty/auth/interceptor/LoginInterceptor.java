3dff2d8c90dbdaec2279d41c14af1dbec4dd71f5
koomchang <leahpar0401@gmail.com>
2024-03-07 12:51:12 +0900

    [feat/#79] :sparkles: Feat: 로그인 인터셉터 구현

--- chatty-be/src/main/java/com/chatty/chatty/auth/interceptor/LoginInterceptor.java
+++ chatty-be/src/main/java/com/chatty/chatty/auth/interceptor/LoginInterceptor.java
@@ -1,2 +1,33 @@
-package com.chatty.chatty.auth.interceptor;public class LoginInterceptor {
+package com.chatty.chatty.auth.interceptor;
+
+import static com.chatty.chatty.auth.exception.AuthExceptionType.UNAUTHORIZED;
+
+import com.chatty.chatty.auth.exception.AuthException;
+import com.chatty.chatty.auth.jwt.JwtUtil;
+import com.chatty.chatty.auth.support.AuthenticationContext;
+import com.chatty.chatty.auth.support.AuthenticationExtractor;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+@RequiredArgsConstructor
+@Component
+@Slf4j
+public class LoginInterceptor implements HandlerInterceptor {
+
+    private final JwtUtil jwtUtil;
+    private final AuthenticationContext authenticationContext;
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
+        String token = AuthenticationExtractor.extract(request)
+                .orElseThrow(() -> new AuthException(UNAUTHORIZED));
+        Long userId = jwtUtil.extract(token);
+        log.info("userId: {}", userId);
+        authenticationContext.setAuthentication(userId);
+        return true;
+    }
 }
