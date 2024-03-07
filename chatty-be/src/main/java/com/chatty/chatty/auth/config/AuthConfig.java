372bd99317737f5d04aa2c0e6fbcf7fc88695f73
koomchang <leahpar0401@gmail.com>
2024-03-07 12:51:51 +0900

    [feat/#79] :sparkles: Feat: interceptor 및 argument resolver 등록을 위한 config 클래스 구현

--- chatty-be/src/main/java/com/chatty/chatty/auth/config/AuthConfig.java
+++ chatty-be/src/main/java/com/chatty/chatty/auth/config/AuthConfig.java
@@ -1,17 +1,38 @@
-package com.chatty.chatty.config;
+package com.chatty.chatty.auth.config;
+
+import static com.chatty.chatty.auth.interceptor.HttpMethod.OPTIONS;
 
 import com.chatty.chatty.auth.controller.AuthArgumentResolver;
+import com.chatty.chatty.auth.interceptor.LoginInterceptor;
+import com.chatty.chatty.auth.interceptor.PathMatcherInterceptor;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @RequiredArgsConstructor
-public class WebConfig implements WebMvcConfigurer {
+public class AuthConfig implements WebMvcConfigurer {
 
     private final AuthArgumentResolver authArgumentResolver;
+    private final LoginInterceptor loginInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(loginInterceptor());
+    }
+
+    /*
+    @AuthUser 어노테이션 사용 시 addPathPattern에 추가해야 함
+     */
+    private HandlerInterceptor loginInterceptor() {
+        return new PathMatcherInterceptor(loginInterceptor)
+                .excludePathPattern("/**", OPTIONS);
+//  예시            .addPathPattern("/test", GET, POST);
+    }
 
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
