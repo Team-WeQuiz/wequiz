dd6ab41d7ebb6057300fb9ac296a36c873ab75d5
koomchang <leahpar0401@gmail.com>
2024-03-16 15:44:34 +0900

    [feat/#54] :sparkles Add: Gson 타임 어댑터 설정 추가

--- chatty-be/src/main/java/com/chatty/chatty/auth/support/GsonLocalDateTimeAdapter.java
+++ chatty-be/src/main/java/com/chatty/chatty/auth/support/GsonLocalDateTimeAdapter.java
@@ -1,2 +1,21 @@
-package com.chatty.chatty.auth.support;public class GsonLocalDateTimeAdapter {
+package com.chatty.chatty.auth.support;
+
+import com.google.gson.*;
+
+import java.lang.reflect.Type;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class GsonLocalDateTimeAdapter implements JsonSerializer<LocalDateTime>, JsonDeserializer<LocalDateTime> {
+
+    @Override
+    public JsonElement serialize(LocalDateTime localDateTime, Type srcType, JsonSerializationContext context) {
+        return new JsonPrimitive(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss").format(localDateTime));
+    }
+
+    @Override
+    public LocalDateTime deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context)
+            throws JsonParseException {
+        return LocalDateTime.parse(json.getAsString(), DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+    }
 }
