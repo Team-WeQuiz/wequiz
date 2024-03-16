63867b61be9a29532e9c9f90a5d7ef4888bf004b
koomchang <leahpar0401@gmail.com>
2024-03-16 15:48:44 +0900

    [feat/#54] :sparkles Feat: 소셜 로그인 서비스를 만들기 위한 인터페이스 구성

--- chatty-be/src/main/java/com/chatty/chatty/auth/service/oauth/SocialLoginService.java
+++ chatty-be/src/main/java/com/chatty/chatty/auth/service/oauth/SocialLoginService.java
@@ -1,2 +1,16 @@
-package com.chatty.chatty.auth.service.oauth;public interface SoicalLoginService {
+package com.chatty.chatty.auth.service.oauth;
+
+import com.chatty.chatty.auth.controller.oauth.dto.SocialAuthResponse;
+import com.chatty.chatty.auth.controller.oauth.dto.SocialUserResponse;
+import com.chatty.chatty.auth.entity.Provider;
+import org.springframework.stereotype.Service;
+
+@Service
+public interface SocialLoginService {
+
+    Provider getServiceName();
+
+    SocialAuthResponse getAccessToken(String authorizationCode);
+
+    SocialUserResponse getUserInfo(String accessToken);
 }
