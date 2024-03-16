372a8d8a5c09529f62926e1a2e536f29eb3a039b
koomchang <leahpar0401@gmail.com>
2024-03-16 15:46:38 +0900

    [feat/#54] :sparkles Feat: openfeign을 이용하여 Kakao 인가 코드 및 유저 정보 가져오는 api 구성

--- chatty-be/src/main/java/com/chatty/chatty/auth/controller/oauth/kakao/KakaoUserApi.java
+++ chatty-be/src/main/java/com/chatty/chatty/auth/controller/oauth/kakao/KakaoUserApi.java
@@ -1,2 +1,15 @@
-package com.chatty.chatty.auth.controller.oauth.kakao;public interface KakaoUserApi {
+package com.chatty.chatty.auth.controller.oauth.kakao;
+
+import com.chatty.chatty.auth.config.FeignConfiguration;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import java.util.Map;
+
+@FeignClient(value = "kakaoUser", url = "https://kapi.kakao.com", configuration = {FeignConfiguration.class})
+public interface KakaoUserApi {
+
+    @GetMapping("/v2/user/me")
+    ResponseEntity<String> getUserInfo(@RequestHeader Map<String, String> header);
 }
