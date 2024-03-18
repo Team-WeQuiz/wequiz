eadbefcf8145dfc762a1e36166e7f7f2e790c3be
koomchang <leahpar0401@gmail.com>
2024-03-18 17:11:43 +0900

    [feat/#53] :sparkles: Feat: 구글 인증 서버에 토큰 요청 및 유저 정보 가져오는 api를 openfeign을 이용하여 구현

--- chatty-be/src/main/java/com/chatty/chatty/auth/controller/oauth/google/GoogleUserApi.java
+++ chatty-be/src/main/java/com/chatty/chatty/auth/controller/oauth/google/GoogleUserApi.java
@@ -1,2 +1,14 @@
-package com.chatty.chatty.auth.controller.oauth.google;public interface GoogleUserApi {
+package com.chatty.chatty.auth.controller.oauth.google;
+
+import com.chatty.chatty.auth.config.FeignConfiguration;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient(value = "googleUser", url = "https://www.googleapis.com", configuration = {FeignConfiguration.class})
+public interface GoogleUserApi {
+
+    @GetMapping("/userinfo/v2/me")
+    ResponseEntity<String> getUserInfo(@RequestParam("access_token") String accessToken);
 }
