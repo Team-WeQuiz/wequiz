05025cb0fe2fc77d14fffc5745a4ce7f55fb56fc
koomchang <leahpar0401@gmail.com>
2024-03-18 17:10:36 +0900

    [feat/#53] :wrench: Config: 구글 Oauth이용을 위한 설정 추가

---
+++ chatty-be/src/main/java/com/chatty/chatty/auth/controller/oauth/google/GoogleUserApi.java
@@ -0,0 +1,2 @@
+package com.chatty.chatty.auth.controller.oauth.google;public interface GoogleUserApi {
+}
---
+++ chatty-be/src/main/java/com/chatty/chatty/auth/service/oauth/GoogleLoginService.java
@@ -0,0 +1,2 @@
+package com.chatty.chatty.auth.service.oauth;public class GoogleLoginService {
+}
