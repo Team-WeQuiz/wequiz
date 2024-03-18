cecb8cfe40b52d0c6a38b5c99724224cd49ed6c8
koomchang <leahpar0401@gmail.com>
2024-03-18 17:13:14 +0900

    [feat/#53] :sparkles: Feat: 구글 인증 서버에 보내느 요청 및 응답 DTO 생성

--- chatty-be/src/main/java/com/chatty/chatty/auth/controller/oauth/dto/GoogleLoginResponse.java
+++ chatty-be/src/main/java/com/chatty/chatty/auth/controller/oauth/dto/GoogleLoginResponse.java
@@ -1,2 +1,16 @@
-package com.chatty.chatty.auth.controller.oauth.dto;public class GoogleLoginResponse {
+package com.chatty.chatty.auth.controller.oauth.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class GoogleLoginResponse {
+
+    private String email;
+    private String picture;
 }
