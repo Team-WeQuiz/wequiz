a965a2db14d925bc66a31475091a80ad43419dd5
ryanbae94 <ryanbae94@gmail.com>
2024-03-20 16:43:16 +0900

    [feat/#114] :sparkles: Add: 구글, 카카오 로그인 구현

---
+++ chatty-fe/src/app/_lib/auth.ts
@@ -0,0 +1,24 @@
+export const kakaoInit = () => {
+  if (window.Kakao && !window.Kakao.isInitialized()) {
+    window.Kakao.init(process.env.NEXT_PUBLIC_KAKAO_JS_KEY);
+    console.log(window.Kakao.isInitialized());
+  }
+};
+
+export const kakaoLogin = (redirectUri: string) => {
+  if (window.Kakao && window.Kakao.isInitialized()) {
+    window.Kakao.Auth.authorize({
+      redirectUri: redirectUri,
+    });
+  } else {
+    console.error("Kakao SDK hasn't been initialized.");
+  }
+};
+
+export const googleLogin = (redirectUri: string) => {
+  window.location.href =
+    `https://accounts.google.com/o/oauth2/auth?client_id=${process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID}&` +
+    `redirect_uri=${redirectUri}&` +
+    'response_type=code&' +
+    'scope=email profile';
+};
