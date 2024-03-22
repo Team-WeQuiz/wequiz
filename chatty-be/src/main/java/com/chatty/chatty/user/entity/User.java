fd16d6a4a3db8edc054c8c86fbf1c0ec09f62475
cherry031 <20203093@kookmin.ac.kr>
2024-03-22 11:14:52 +0900

    [feat/#149] :recycle: Refactor: User의 password, Player의 userId는 null로 수정

--- chatty-be/src/main/java/com/chatty/chatty/user/entity/User.java
+++ chatty-be/src/main/java/com/chatty/chatty/user/entity/User.java
@@ -47,7 +47,7 @@ public class User extends BaseEntity {
     private String profileImage;
 
     @Enumerated(EnumType.STRING)
-    @Column
+    @Column(nullable = false)
     private Provider loginType;
 
     @PrePersist
