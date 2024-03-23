fa827b37ce819d68b5cc0429488d21da7e17f5a8
ryanbae94 <ryanbae94@gmail.com>
2024-03-23 21:25:25 +0900

    [feat/#128] :recycle: refactor: api 레이어 구현, 코드 적용

--- chatty-fe/src/app/(auth)/sign-in/google/callback/page.tsx
+++ chatty-fe/src/app/(auth)/sign-in/google/callback/page.tsx
@@ -1,27 +1,28 @@
 'use client';
 
-import { authHandler } from '@/app/_lib/auth';
-import { useSearchParams } from 'next/navigation';
+import { postGoogleLogin } from '@/app/_api/auth';
+import useAuthStore from '@/app/_store/useAuthStore';
+import { useRouter, useSearchParams } from 'next/navigation';
 
 import React, { Suspense, useEffect } from 'react';
 
 function GoogleLoginComponent() {
   const searchParams = useSearchParams();
   const authCode = searchParams.get('code');
+  const { setTokens } = useAuthStore();
+  const router = useRouter();
 
-  const handleSubmit = async (code: string | string[]) => {
-    authHandler(
-      { code: code },
-      'http://localhost:8080/api/oauth/google/signIn',
-    );
+  const handleLogin = async (authCode: string) => {
+    const response = await postGoogleLogin(authCode);
+    const { accessToken, refreshToken } = response;
+    setTokens(accessToken, refreshToken);
+    localStorage.setItem('accessToken', accessToken);
+    localStorage.setItem('refreshToken', refreshToken);
+    router.push('/main-lobby');
   };
 
   useEffect(() => {
-    if (authCode) {
-      const decoded = decodeURI(authCode);
-      console.log(decoded);
-      handleSubmit(decoded);
-    }
+    if (authCode) handleLogin(authCode);
   });
 
   return <div>로그인 중입니다..</div>;
