4c28b0223995f7c5d5bb71ed930042ed843b811d
cherry031 <20203093@kookmin.ac.kr>
2024-03-28 15:35:48 +0900

    [feat/#169] :recycle: Refactor: quizRoom 관련 DTO id에서 roomId로 변경

--- chatty-be/src/main/java/com/chatty/chatty/model/controller/dto/MakeQuizResponse.java
+++ chatty-be/src/main/java/com/chatty/chatty/model/controller/dto/MakeQuizResponse.java
@@ -5,7 +5,7 @@ import lombok.Builder;
 @Builder
 public record MakeQuizResponse(
 
-        Long id,
+        Long roomId,
 
         Long quizDocId
 ) {
--- chatty-be/src/main/java/com/chatty/chatty/quizroom/controller/dto/MakeRoomResponse.java
+++ chatty-be/src/main/java/com/chatty/chatty/quizroom/controller/dto/MakeRoomResponse.java
@@ -5,7 +5,7 @@ import lombok.Builder;
 @Builder
 public record MakeRoomResponse(
 
-        Long id
+        Long roomId
 ) {
 
 }
