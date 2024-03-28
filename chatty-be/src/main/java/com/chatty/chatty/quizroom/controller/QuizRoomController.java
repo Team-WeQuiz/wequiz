395d0a09ae2deb62f7f0555edceade2322b26cfe
cherry031 <20203093@kookmin.ac.kr>
2024-03-28 14:53:08 +0900

    [feat/#169] :recycle: Refactor: Quizroom Entity에 quizDocId 컬럼 추가

--- chatty-be/src/main/java/com/chatty/chatty/quizroom/controller/QuizRoomController.java
+++ chatty-be/src/main/java/com/chatty/chatty/quizroom/controller/QuizRoomController.java
@@ -5,11 +5,9 @@ import com.chatty.chatty.quizroom.controller.dto.MakeRoomResponse;
 import com.chatty.chatty.quizroom.entity.QuizRoom;
 import com.chatty.chatty.quizroom.service.QuizRoomService;
 import java.util.List;
-import java.util.Optional;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -29,11 +27,6 @@ public class QuizRoomController {
     public ResponseEntity<List<QuizRoom>> rooms() {
         List<QuizRoom> rooms = quizRoomService.findAll();
         return ResponseEntity.status(HttpStatus.OK).body(rooms);
-    }
-
-    @GetMapping("/{roomId}")
-    public ResponseEntity<Optional<QuizRoom>> findRoom(@PathVariable Long id) {
-        return ResponseEntity.status(HttpStatus.OK).body(quizRoomService.findById(id));
     }
 
     @PostMapping
