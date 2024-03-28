dd6479569b54e6ccd3a618d7f5b6b7a9e575a495
cherry031 <20203093@kookmin.ac.kr>
2024-03-28 14:19:35 +0900

    [feat/#169] :recycle: Refactor: 방 생성 dto 변경

--- chatty-be/src/main/java/com/chatty/chatty/quizroom/controller/dto/MakeRoomResponse.java
+++ chatty-be/src/main/java/com/chatty/chatty/quizroom/controller/dto/MakeRoomResponse.java
@@ -1,16 +1,11 @@
 package com.chatty.chatty.quizroom.controller.dto;
 
-import java.util.List;
 import lombok.Builder;
 
 @Builder
 public record MakeRoomResponse(
 
-        Long id,
-
-        List<String> questions,
-
-        String description
+        Long id
 ) {
 
 }
