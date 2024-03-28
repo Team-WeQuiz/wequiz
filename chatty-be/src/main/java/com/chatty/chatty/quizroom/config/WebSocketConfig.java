8f050975fd8976b94aa42a06160e254b1ad63621
cherry031 <20203093@kookmin.ac.kr>
2024-03-28 15:40:49 +0900

    [feat/#169] :recycle: Refactor: TODO 주석 추가

--- chatty-be/src/main/java/com/chatty/chatty/quizroom/config/WebSocketConfig.java
+++ chatty-be/src/main/java/com/chatty/chatty/quizroom/config/WebSocketConfig.java
@@ -14,7 +14,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
                 .setAllowedOriginPatterns("*");
-        // .withSockJS();
+        // TODO .withSockJS();
     }
 
     @Override
