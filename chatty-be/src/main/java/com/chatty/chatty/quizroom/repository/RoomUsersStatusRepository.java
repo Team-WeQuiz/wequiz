6f5e18079ab4b7396655f89a89c5912ecbc365e3
koomchang <leahpar0401@gmail.com>
2024-04-02 13:07:27 +0900

    [feat/#175] :sparkles: Feat: 게임방 입장 비즈니스 로직 구현

--- chatty-be/src/main/java/com/chatty/chatty/quizroom/repository/RoomUsersStatusRepository.java
+++ chatty-be/src/main/java/com/chatty/chatty/quizroom/repository/RoomUsersStatusRepository.java
@@ -1,19 +1,35 @@
 package com.chatty.chatty.quizroom.repository;
 
+import static com.chatty.chatty.quizroom.exception.QuizRoomExceptionType.ROOM_NOT_FOUND;
+
 import com.chatty.chatty.quizroom.controller.dto.RoomUserStatus;
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
+import com.chatty.chatty.quizroom.controller.dto.RoomUsersStatus;
+import com.chatty.chatty.quizroom.exception.QuizRoomException;
+import java.util.ArrayList;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 @Repository
+@RequiredArgsConstructor
 public class RoomUsersStatusRepository {
 
-    private static final Map<Long, RoomUserStatus> roomUsersStatus = new ConcurrentHashMap<>();
+    private static final List<RoomUsersStatus> roomUsersStatuses = new ArrayList<>();
 
-    public void addUserToRoom(Long roomId, Long userId) {
-        RoomUserStatus roomUserStatus = RoomUserStatus.builder()
+    public RoomUsersStatus findByRoomId(Long roomId) {
+        return roomUsersStatuses.stream()
+                .filter(roomUsersStatus -> roomUsersStatus.roomId().equals(roomId))
+                .findFirst()
+                .orElseThrow(() -> new QuizRoomException(ROOM_NOT_FOUND));
+    }
+
+    public RoomUsersStatus addUserToRoom(Long roomId, Long userId) {
+        RoomUsersStatus currentStatus = findByRoomId(roomId);
+        RoomUserStatus newUser = RoomUserStatus.builder()
                 .userId(userId)
+                .isReady(false)
                 .build();
-        roomUsersStatus.put(roomId, roomUserStatus);
+        currentStatus.roomUserStatuses().add(newUser);
+        return currentStatus;
     }
 }
--- chatty-be/src/main/java/com/chatty/chatty/quizroom/service/GameService.java
+++ chatty-be/src/main/java/com/chatty/chatty/quizroom/service/GameService.java
@@ -1,12 +1,17 @@
 package com.chatty.chatty.quizroom.service;
 
-import com.chatty.chatty.quizroom.controller.dto.RoomUsersStatusResponse;
+import com.chatty.chatty.quizroom.controller.dto.RoomUsersStatus;
+import com.chatty.chatty.quizroom.repository.RoomUsersStatusRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
-public class SocketService {
+@RequiredArgsConstructor
+public class GameService {
 
-    public RoomUsersStatusResponse join(Long roomId, String userId) {
+    private final RoomUsersStatusRepository roomUsersStatusRepository;
 
+    public RoomUsersStatus join(Long roomId, Long userId) {
+        return roomUsersStatusRepository.addUserToRoom(roomId, userId);
     }
 }
