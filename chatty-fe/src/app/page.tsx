a66cabb2fb2e9e4bba93954cfdf4f7ba07ebf87d
ryanbae94 <ryanbae94@gmail.com>
2024-04-04 02:11:38 +0900

    [feat/#177] :recycle: Refactor: 메인화면 초기화

--- chatty-fe/src/app/page.tsx
+++ chatty-fe/src/app/page.tsx
@@ -1,12 +1,3 @@
-'use client';
-
-import { useEffect } from 'react';
-import { useRouter } from 'next/navigation';
-
 export default function Home() {
-  const navigate = useRouter();
-  useEffect(() => {
-    navigate.push('/main');
-  }, []);
-  return <div></div>;
+  return <div>MAIN</div>;
 }
