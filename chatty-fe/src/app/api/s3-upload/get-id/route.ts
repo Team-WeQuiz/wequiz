da2f86cc08978a9c68efd276eed2141461ab2b59
ryanbae94 <ryanbae94@gmail.com>
2024-04-04 02:09:16 +0900

    [feat/#177] :recycle: Refactor: 환경변수 제거 및 key 가져오는 코드 추가

--- chatty-fe/src/app/api/s3-upload/get-id/route.ts
+++ chatty-fe/src/app/api/s3-upload/get-id/route.ts
@@ -1,19 +1,22 @@
+import { getAWSAccessKeys } from '@/app/_lib/keys';
 import { S3Client, CreateMultipartUploadCommand } from '@aws-sdk/client-s3';
 import { NextResponse } from 'next/server';
-
-const Bucket = process.env.NEXT_PUBLIC_AWS_BUCKET_NAME as string;
-const s3 = new S3Client({
-  region: process.env.NEXT_PUBLIC_AWS_REGION,
-  credentials: {
-    accessKeyId: process.env.NEXT_PUBLIC_AWS_ACCESS_KEY as string,
-    secretAccessKey: process.env.NEXT_PUBLIC_AWS_SECRET_KEY as string,
-  },
-});
 
 export async function POST(request: Request) {
   const body = await request.json();
 
   try {
+    const keys = await getAWSAccessKeys();
+
+    const Bucket = 'kyuyeon-test';
+    const region = 'ap-northeast-2';
+    const s3 = new S3Client({
+      region,
+      credentials: {
+        accessKeyId: keys.accessKey,
+        secretAccessKey: keys.secretAccessKey,
+      },
+    });
     const command = new CreateMultipartUploadCommand({
       Bucket,
       Key: body.data.fileName,
