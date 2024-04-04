e2f503b2b68abf065b3ddef032fc18386a067aaf
ryanbae94 <ryanbae94@gmail.com>
2024-04-04 13:24:44 +0900

    [feat/#177] :recycle: Refactor: 함수명 변경 getAWSKeys -> getSecretKeys

--- chatty-fe/src/app/_lib/keys.ts
+++ chatty-fe/src/app/_lib/keys.ts
@@ -1,17 +1,17 @@
-import { getAWSKeys } from '../_api/auth';
+import { getSecretKeys } from '../_api/auth';
 
 export const getGoogleKeys = async () => {
-  const data = await getAWSKeys();
+  const data = await getSecretKeys();
   return data.GOOGLE_CLIENT_ID;
 };
 
 export const getKakaoKeys = async () => {
-  const data = await getAWSKeys();
+  const data = await getSecretKeys();
   return data.KAKAO_JS_KEY;
 };
 
 export const getAWSAccessKeys = async () => {
-  const data = await getAWSKeys();
+  const data = await getSecretKeys();
   const accessKey = data.AWS_ACCESS_KEY;
   const secretAccessKey = data.AWS_SECRET_KEY;
   return { accessKey, secretAccessKey };
