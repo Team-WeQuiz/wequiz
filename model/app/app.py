604278a5a1f9f0a8b0f56b889eed620ea2baf60f
noooey <20203065@kookmin.ac.kr>
2024-04-05 21:57:23 +0900

    [feat/#195] :recycle: Refactor: 문제 생성 API 수정, DB 재연결

--- model/app/app.py
+++ model/app/app.py
@@ -12,16 +12,10 @@ import boto3
 
 app = FastAPI()
 
-# # Pydantic model for request body validation
-# class ProbRequest(BaseModel):
-#     timestamp: str
-#     id: int
-#     message: str
-#     indices: object
-#     type: str  # "객관식", "주관식", "단답형"
 
 class GenerateRequest(BaseModel):
     id: int
+    timestamp: str
     numOfQuiz: int
     type: str
     files: List[str]
@@ -77,83 +71,58 @@ def generate(generate_request: GenerateRequest):
         log('error', f'Failed to Summarize PDF: {str(e)}', AWS_ACCESS_KEY)
         raise e
 
-    #### generate quiz ###
+    #### generate quiz ####
     prob_generator = ProbGenerator(split_docs, OPENAI_API_KEY)
     #### NotImplemented keyword part ####
-    keywords = ["원핫인코딩"] * generate_request.numOfQuiz
+    keywords = ["원핫인코딩", "의미기반 언어모델", "사전학습", "전처리", "미세조정"]
+    if len(keywords) != generate_request.numOfQuiz:
+        raise Exception('키워드가 충분히 생성되지 않았습니다.')
     questions = []
     #####################################
     for idx, keyword in enumerate(keywords):
         try:
             generated_prob = prob_generator.generate(generate_request.type, keyword, idx+1)
             questions.append(generated_prob)
-            yield generated_prob
         except Exception as e:
             log('error', f'Failed to Generate Quiz {idx}-{keyword}: {str(e)}', AWS_ACCESS_KEY)
             raise e
 
-    ### TODO ###
-    # dynamoDB 스키마 형턔로 바꿔주는 func
-    # dynamoDB에 push하며 끝
+    # DynamoDB에 삽입할 데이터 구성
+    data = {
+        "id": {"N": str(generate_request.id)},  # id를 문자열로 변환하여 저장
+        "timestamp": {"S": generate_request.timestamp},  # 변환하지 않고 그대로 사용
+        "questions": {"L": [
+            {
+                "M": {
+                    "id": {"S": str(question["id"])},
+                    "question_number": {"N": str(question["question_number"])},
+                    "type": {"S": question["type"]},
+                    "question": {"S": question["question"]},
+                    "options": {"L": [{"S": option} for option in question["options"]]},
+                    "answer": {"S": question["answer"]}
+                }
+            } for question in questions  # Assuming `questions` is a list of question objects
+        ]},
+        "description": {"S": summary}
+    }
 
-# unused
-# def generate_prob(prob: ProbRequest):
-#     try:
-#         chain = Chain(prob.indices, prob.type, OPENAI_API_KEY)
-#         response = chain.prob(prob.message)
-#         log('info', f'Prob Chain inference is successed.', AWS_ACCESS_KEY)
+    try:
+        # DynamoDB에 데이터 삽입
+        dynamodb.put_item(
+            TableName=TABLE_NAME,
+            Item=data
+        )
 
-#         print(f'prob.timestamp: {prob.timestamp}')
+        res = {
+            "id": generate_request.id
+        }
 
-#         # DynamoDB에 삽입할 데이터 구성
-#         data = {
-#             "id": {"S": str(prob.id)},  # id를 문자열로 변환하여 저장
-#             "timestamp": {"S": prob.timestamp},  # 변환하지 않고 그대로 사용
-#             "day": {"N": str(datetime.datetime.strptime(prob.timestamp, "%Y-%m-%d %H:%M:%S").day)},
-#             "month": {"N": str(datetime.datetime.strptime(prob.timestamp, "%Y-%m-%d %H:%M:%S").month)},
-#             "year": {"N": str(datetime.datetime.strptime(prob.timestamp, "%Y-%m-%d %H:%M:%S").year)},
-#             "questions": {"L": [
-#                 {
-#                     "M": {
-#                         "id": {"S": str(uuid.uuid4())},
-#                         "question_number": {"N": "1"},
-#                         "type": {"S": str(prob.type)}, 
-#                         "question": {"S": response["text"]["question"]},
-#                         "options": {"L": [{"S": option} for option in response["text"]["choices"]]},
-#                         "answer": {"S": response["text"]["answer"]}
-#                     }
-#                 }
-#             ]},
-#             "description": {"S": 'not yet'}
-#         }
-
-#         # DynamoDB에 데이터 삽입
-#         dynamodb.put_item(
-#             TableName=TABLE_NAME,
-#             Item=data
-#         )
-
-#         res = {
-#             "id": prob.id,
-#             "questions":
-#             [
-#                 {
-#                     "id": uuid.uuid4(),
-#                     "question_number": 1,
-#                     "type": prob.type,  # 1:객, 2:주, 3:단
-#                     "question": response["text"]["question"],
-#                     "options": response["text"]["choices"],
-#                     "answer": response["text"]["answer"]
-#                 },
-#             ],
-#             "description": 'not yet',
-#         }
+        yield res
+    except Exception as e:
+        log('error', f'Failed to push quiz to dynamodb: {str(e)}', AWS_ACCESS_KEY)
+        raise e
 
 
-#         return res
-#     except Exception as e:
-#         log('error', f'Failed to Prob Chain Inference: {str(e)}', AWS_ACCESS_KEY)
-#         raise e
 
 if __name__ == "__main__":
     import uvicorn
