8943458b9f98c4f564f89edb0166d1902a45e0db
Woorim Kim <wrkimlimi@gmail.com>
2024-04-08 02:36:16 +0900

    [feat/#171] ♻️ Refact: Pass userStatus including isReady to UserCard

--- chatty-fe/src/app/(game)/waiting-room/[id]/_components/UserList/UserList.tsx
+++ chatty-fe/src/app/(game)/waiting-room/[id]/_components/UserList/UserList.tsx
@@ -5,20 +5,14 @@ import * as styles from './UserList.css';
 import useWaitingStore from '@/app/_store/useWaitingStore';
 
 const UserList = () => {
-  const {userStatuses} = useWaitingStore();
+  const { userStatuses } = useWaitingStore();
 
   return (
     <div className={styles.mainContainer}>
       {userStatuses &&
         userStatuses.map(
           (user) =>
-            user.userId && (
-              <UserCard
-                key={user.userId}
-                userId={user.userId}
-                message={user.message}
-              />
-            ),
+            user.userId && <UserCard key={user.userId} userStatus={user} />,
         )}
     </div>
   );
