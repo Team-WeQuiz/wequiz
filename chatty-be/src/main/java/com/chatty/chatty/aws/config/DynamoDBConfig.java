b86a00e4f25a6597458d81aa1bf7cdd231428000
cherry031 <20203093@kookmin.ac.kr>
2024-04-11 04:12:18 +0900

    [Feat/#217] :recycle: Refactor: DynamoDB 데이터 읽어오는 방법 load에서 getItem으로 수정

--- chatty-be/src/main/java/com/chatty/chatty/aws/config/DynamoDBConfig.java
+++ chatty-be/src/main/java/com/chatty/chatty/aws/config/DynamoDBConfig.java
@@ -2,9 +2,8 @@ package com.chatty.chatty.aws.config;
 
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
-import com.amazonaws.services.dynamodbv2.AmazonDynamoDBAsyncClientBuilder;
-import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient;
-import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,15 +15,11 @@ public class DynamoDBConfig {
     private final AWSKey awsKey;
 
     @Bean
-    public DynamoDBMapper dynamoDBMapper() {
-        return new DynamoDBMapper(amazonDynamoDBClient());
-    }
-
-    private AmazonDynamoDBClient amazonDynamoDBClient() {
-        BasicAWSCredentials awsCredentials = new BasicAWSCredentials(awsKey.getAccessKey(), awsKey.getSecretKey());
-        return (AmazonDynamoDBClient) AmazonDynamoDBAsyncClientBuilder.standard()
+    public AmazonDynamoDB amazonDynamoDB() {
+        return AmazonDynamoDBClientBuilder.standard()
+                .withCredentials(
+                        new AWSStaticCredentialsProvider(new BasicAWSCredentials(awsKey.getAccessKey(), awsKey.getSecretKey())))
                 .withRegion(awsKey.getRegion())
-                .withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
                 .build();
     }
 }
