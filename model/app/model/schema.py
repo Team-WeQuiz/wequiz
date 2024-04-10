38ff595cfa803b84b20e24df508de84ad3a9864e
noooey <20203065@kookmin.ac.kr>
2024-04-10 15:26:28 +0900

    [feat/#201] :sparkles: Feat: 단답형 스키마, 프롬프트 추가

--- model/app/model/schema.py
+++ model/app/model/schema.py
@@ -2,7 +2,12 @@ from langchain_core.pydantic_v1 import BaseModel, Field
 from typing import List
 
 # 문제 형식 정의
-class Output(BaseModel):
+class ChoiceOutput(BaseModel):
     question: str = Field(description="question about keywords")
     choices: List[str] = Field(description="Five options for the problem. Among these, there is only one correct answer.")
     answer: str = Field(description="The correct option among the given choices")
+
+class ShortOutput(BaseModel):
+    question: str = Field(description="question about keywords")
+    choices: List[str] = []  # 빈 리스트로 기본값 설정
+    answer: str = Field(description="only a word as a correct answer.")
