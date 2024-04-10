49cd0fa924404fb8d0ca60d35b73c304fdbe3b17
noooey <20203065@kookmin.ac.kr>
2024-04-10 15:26:28 +0900

    [feat/#201] :sparkles: Feat: 단답형 스키마, 프롬프트 추가

--- model/app/model/prompt.py
+++ model/app/model/prompt.py
@@ -1,7 +1,20 @@
-PROB_TEMPLATE = """
+CHOICE_PROB_TEMPLATE = """
 You are the best kindly AI model creating questions for learning.
 
 Please create a multiple choice question about keyword based on the document.
+Be sure to answer in Korean.
+keyword: {message}
+
+{format_instructions}
+
+YOUR ANSWER:"""
+
+######################################################################
+SHORT_PROB_TEMPLATE = """
+You are the best kindly AI model creating questions for learning.
+
+Please create a short answer question that people can answer as a word about keyword based on the document.
+The correct answer is only a word.
 Be sure to answer in Korean.
 keyword: {message}
 
