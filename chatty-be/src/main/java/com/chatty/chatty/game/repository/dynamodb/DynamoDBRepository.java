7f6ec1770fa2600a7b33503b4cf8664116233b7b
cherry031 <20203093@kookmin.ac.kr>
2024-04-12 11:36:49 +0900

    [Feat/#221] :recycle: Refactor: dynamoDB에서 항목을 읽어오는 작업을 repository로 이동

---
+++ chatty-be/src/main/java/com/chatty/chatty/game/repository/dynamodb/DynamoDBRepository.java
@@ -0,0 +1,38 @@
+package com.chatty.chatty.game.repository.dynamodb;
+
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.document.DynamoDB;
+import com.amazonaws.services.dynamodbv2.document.Item;
+import com.amazonaws.services.dynamodbv2.document.Table;
+import com.amazonaws.services.dynamodbv2.document.spec.GetItemSpec;
+import java.util.List;
+import java.util.Map;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class DynamoDBRepository {
+
+    private static final String TABLE_NAME = "wequiz-quiz";
+    private static final String HASH_KEY = "id";
+    private static final String RANGE_KEY = "timestamp";
+    private static final String QUESTIONS_ATTR_NAME = "questions";
+
+    private final AmazonDynamoDB amazonDynamoDB;
+    private final DynamoDB dynamoDB;
+
+    public DynamoDBRepository(AmazonDynamoDB amazonDynamoDB) {
+        this.amazonDynamoDB = amazonDynamoDB;
+        this.dynamoDB = new DynamoDB(amazonDynamoDB);
+    }
+    
+    public List<Map<String, Object>> getQuizFromDB(Long itemId, String timestamp) {
+        Table table = dynamoDB.getTable(TABLE_NAME);
+        GetItemSpec spec = new GetItemSpec()
+                .withPrimaryKey(HASH_KEY, itemId, RANGE_KEY, timestamp)
+                .withProjectionExpression(QUESTIONS_ATTR_NAME);
+
+        Item item = table.getItem(spec);
+
+        return item.getList(QUESTIONS_ATTR_NAME);
+    }
+}
--- chatty-be/src/main/java/com/chatty/chatty/game/service/dynamodb/DynamoDBService.java
+++ chatty-be/src/main/java/com/chatty/chatty/game/service/dynamodb/DynamoDBService.java
@@ -1,42 +1,22 @@
 package com.chatty.chatty.game.service.dynamodb;
 
-import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
-import com.amazonaws.services.dynamodbv2.document.DynamoDB;
-import com.amazonaws.services.dynamodbv2.document.Item;
-import com.amazonaws.services.dynamodbv2.document.Table;
-import com.amazonaws.services.dynamodbv2.document.spec.GetItemSpec;
 import com.chatty.chatty.game.controller.dto.dynamodb.Question;
+import com.chatty.chatty.game.repository.dynamodb.DynamoDBRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class DynamoDBService {
 
-    private static final String TABLE_NAME = "wequiz-quiz";
-    private static final String HASH_KEY = "id";
-    private static final String RANGE_KEY = "timestamp";
-    private static final String QUESTIONS_ATTR_NAME = "questions";
-
-    private final AmazonDynamoDB amazonDynamoDB;
-    private final DynamoDB dynamoDB;
-
-    public DynamoDBService(AmazonDynamoDB amazonDynamoDB) {
-        this.amazonDynamoDB = amazonDynamoDB;
-        this.dynamoDB = new DynamoDB(amazonDynamoDB);
-    }
+    private final DynamoDBRepository dynamoDBRepository;
 
     public List<Question> getQuizFromDB(Long itemId, String timestamp) {
-        Table table = dynamoDB.getTable(TABLE_NAME);
-        GetItemSpec spec = new GetItemSpec()
-                .withPrimaryKey(HASH_KEY, itemId, RANGE_KEY, timestamp)
-                .withProjectionExpression(QUESTIONS_ATTR_NAME);
-
-        Item item = table.getItem(spec);
-
-        List<Map<String, Object>> questions = item.getList(QUESTIONS_ATTR_NAME);
+        List<Map<String, Object>> questions = dynamoDBRepository.getQuizFromDB(itemId, timestamp);
         return convertToList(questions);
     }
 
