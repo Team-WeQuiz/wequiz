cdc1cf2ff56f5424d249425bfdf65ffda3b37674
cherry031 <20203093@kookmin.ac.kr>
2024-04-18 17:31:45 +0900

    [feat/#221] :wrench: Add: 소켓 일대일 구독 경로 추가

--- chatty-be/src/main/java/com/chatty/chatty/config/WebSocketConfig.java
+++ chatty-be/src/main/java/com/chatty/chatty/config/WebSocketConfig.java
@@ -28,7 +28,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
-        registry.enableSimpleBroker("/sub");
+        registry.enableSimpleBroker("/sub", "/queue");
         registry.setApplicationDestinationPrefixes("/pub");
     }
 
