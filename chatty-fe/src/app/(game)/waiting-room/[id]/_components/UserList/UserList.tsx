5014d00874902c460c067cbbb23c73964a1f1188
Woorim Kim <wrkimlimi@gmail.com>
2024-04-24 17:06:17 +0900

    [feat/#268] :sparkles: Feat: Add quiz bot to user list

--- chatty-fe/src/app/(game)/waiting-room/[id]/_components/UserList/UserList.tsx
+++ chatty-fe/src/app/(game)/waiting-room/[id]/_components/UserList/UserList.tsx
@@ -1,14 +1,33 @@
 'use client';
 
+import { useEffect, useState } from 'react';
 import UserCard from '../UserCard/UserCard';
 import * as styles from './UserList.css';
 import useWaitingStore from '@/app/_store/useWaitingStore';
+import { UserStatus } from '@/app/_types/WaitingStatus';
 
-const UserList = () => {
+const UserList = ({ isQuizReady }: { isQuizReady: boolean }) => {
   const { userStatuses } = useWaitingStore();
+
+  const [botStatus, setBotStatus] = useState<UserStatus>({
+    userId: -1,
+    isReady: isQuizReady,
+  });
+
+  useEffect(() => {
+    if (isQuizReady) {
+      setBotStatus({
+        userId: -1,
+        isReady: isQuizReady,
+        message: '나도 준비 완료!',
+      });
+    }
+  }, [isQuizReady]);
 
   return (
     <div className={styles.mainContainer}>
+      {/* 문제 생성 확인 봇 */}
+      <UserCard userStatus={botStatus} />
       {userStatuses &&
         userStatuses.map(
           (user) =>
