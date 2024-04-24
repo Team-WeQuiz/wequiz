2f93f790d040ba87dc26c101ea5766b41f34f01e
koomchang <leahpar0401@gmail.com>
2024-04-24 20:57:59 +0900

    [feat/#267] :sparkles: Feat: 스프링에서 비동기 처리를 위한 config 설정

---
+++ chatty-be/src/main/java/com/chatty/chatty/config/AsyncConfig.java
@@ -0,0 +1,10 @@
+package com.chatty.chatty.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@Configuration
+@EnableAsync
+public class AsyncConfig {
+
+}
