8f7cde5998cbdcda9d1b487f7823bd273e543e3b
Woorim Kim <wrkimlimi@gmail.com>
2024-05-02 01:36:32 +0900

    [feat/#280] :sparkles: Feat: styling nickname modal

--- chatty-fe/src/app/(game)/enter-room/[id]/_component/NicknameModal/NicknameModal.tsx
+++ chatty-fe/src/app/(game)/enter-room/[id]/_component/NicknameModal/NicknameModal.tsx
@@ -1,5 +1,11 @@
+'use client';
+
+import { useState } from 'react';
 import Modal from '@/app/_components/Modal/Modal';
-import { useEffect } from 'react';
+import * as styles from './NicknameModal.css';
+import Image from 'next/image';
+import TextInputField from '@/app/_components/TextInputField';
+import GradButton from '@/app/_components/GradButton';
 
 const NicknameModal = ({
   name,
@@ -10,13 +16,39 @@ const NicknameModal = ({
   isOpen: boolean;
   onClose: () => void;
 }) => {
-  useEffect(() => {
-    console.log('inside nickname modal isOpen: ', isOpen);
-  }, [isOpen]);
+  const [nickname, setNickname] = useState('');
+
   return (
     <Modal isOpen={isOpen} onClose={onClose}>
-      <div>{name}</div>
-      <div>닉네임을 설정해 주세요.</div>
+      <div className={styles.modalContent}>
+        <div className={styles.titleContainer}>
+          <Image
+            src={'/images/DecoIcon.svg'}
+            alt={'quiz'}
+            width={90}
+            height={90}
+          />
+          <div className={styles.titleWrapper}>{name}</div>
+        </div>
+        <div className={styles.inputContainer}>
+          <Image src={'/images/User.svg'} alt={'user'} width={24} height={24} />
+          <div className={styles.inputWrapper}>
+            <TextInputField
+              value={nickname}
+              placeholder={'닉네임을 설정해 주세요.'}
+              onChange={(e) => {
+                setNickname(e.target.value);
+              }}
+              borderRadius={12}
+            />
+          </div>
+        </div>
+        <div className={styles.buttonContainer}>
+          <GradButton color="primary" fullWidth rounded>
+            <span className={styles.buttonText}>다음</span>
+          </GradButton>
+        </div>
+      </div>
     </Modal>
   );
 };
