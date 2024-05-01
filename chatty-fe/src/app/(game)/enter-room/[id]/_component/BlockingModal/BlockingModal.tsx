7a64b3cdece648321a29057696d373027e81eb28
Woorim Kim <wrkimlimi@gmail.com>
2024-05-02 01:46:14 +0900

    [feat/#280] :sparkles: Feat: styling blocking modal

--- chatty-fe/src/app/(game)/enter-room/[id]/_component/BlockingModal/BlockingModal.tsx
+++ chatty-fe/src/app/(game)/enter-room/[id]/_component/BlockingModal/BlockingModal.tsx
@@ -1,5 +1,6 @@
 import Modal from '@/app/_components/Modal/Modal';
-import { useEffect } from 'react';
+import * as styles from './BlockingModal.css';
+import Image from 'next/image';
 
 const BlockingModal = ({
   name,
@@ -10,12 +11,34 @@ const BlockingModal = ({
   isOpen: boolean;
   onClose: () => void;
 }) => {
-  useEffect(() => {}, [isOpen]);
-
   return (
     <Modal isOpen={isOpen} onClose={onClose}>
-      <div>{name}</div>
-      <div>방이 다 찼습니다!</div>
+      <div className={styles.modalContent}>
+        <div className={styles.titleContainer}>
+          <Image
+            src={'/images/DecoIcon.svg'}
+            alt={'quiz'}
+            width={90}
+            height={90}
+          />
+          <div className={styles.titleWrapper}>{name}</div>
+        </div>
+        <div className={styles.textContainer}>
+          <Image
+            src={'/images/Cancel_fill.svg'}
+            alt={'cancel'}
+            width={32}
+            height={32}
+          />
+          <span>방이 다 챴습니다!</span>
+          <Image
+            src={'/images/Cancel_fill.svg'}
+            alt={'cancel'}
+            width={32}
+            height={32}
+          />
+        </div>
+      </div>
     </Modal>
   );
 };
