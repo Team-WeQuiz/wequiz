879119ceb80dbd3298b31e0f44be3c074d889642
noooey <20203065@kookmin.ac.kr>
2024-05-01 23:45:11 +0900

    [feat/#296] :zap: Improv: 스키마 수정

--- model/app/model/schema.py
+++ model/app/model/schema.py
@@ -3,11 +3,11 @@ from typing import List
 
 # 문제 형식 정의
 class ChoiceOutput(BaseModel):
-    question: str = Field(description="question about keywords")
+    question: str = Field(description="question about context")
     choices: List[str] = Field(description="Five options for the problem. Among these, there is only one correct answer.")
     correct: str = Field(description="The correct option among the given choices")
 
 class ShortOutput(BaseModel):
-    question: str = Field(description="question about keywords")
-    choices: List[str] = []  # 빈 리스트로 기본값 설정
+    question: str = Field(description="question about context")
+    choices: List[str] = Field(description="please only empty list")  # 빈 리스트로 기본값 설정
     correct: str = Field(description="only a word as a correct answer.")
