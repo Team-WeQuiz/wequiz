c7e96c0d68aac45681ebefab636d56364c966a41
noooey <20203065@kookmin.ac.kr>
2024-05-04 18:33:31 +0900

    [feat/#312] :recycle: Refactor: quizgenerator에 벡터화 추가

--- model/app/data/generator.py
+++ model/app/data/generator.py
@@ -1,45 +1,48 @@
+import uuid, random
+from langchain_community.vectorstores import FAISS
+from langchain_openai import OpenAIEmbeddings
 from model.chain import Chain
-from data.preprocessor import Vectorizer
 from data.settings import QUIZ_GENERATE_RETRY
-import uuid
+from utils.logger import *
+# 로깅 설정
+setup_logging()
 
 class QuizGenerator():
     def __init__(self, split_docs):
-        self.vectorizer = Vectorizer()
+        self.embedding = OpenAIEmbeddings(model="text-embedding-3-large")
         try:
-            self.indices = self.vectorizer.convert(split_docs)
+            self.indices = FAISS.from_documents(split_docs, self.embedding)
         except Exception as e:
             raise e
         self.chain = Chain(self.indices)
     
-    def generate(self, type, keyword, question_number):
+    def generate(self, keyword, question_number):
         retry = 0
-        types = ["객관식", "단답형", "OX퀴즈"]
-        if type in [0, 1]: # 객관식, 단답형
-            while retry < QUIZ_GENERATE_RETRY:
-                try:
-                    response = self.chain.prob(type, keyword)
-                    data = {
-                        "id": f'quiz-{uuid.uuid4()}',
-                        "question_number": question_number,
-                        "type": types[type],
-                        "question": response["text"]["question"],
-                        "options": response["text"]["choices"],
-                        "correct": response["text"]["correct"]
-                    }
-                    break
-                except KeyError:
-                    print("retry generating quiz due to KeyError")
-                    retry += 1
-                    continue
-                except Exception as e:
-                    print(f"An unexpected quiz generate error occurred: {e}")
-                    retry += 1
-                    continue
-            return data
-        
-        else:
-            raise NotImplementedError("OX퀴즈에 대한 chain을 구성해야합니다.")
+        types = ["객관식", "단답형"]
+        # Randomly select type (0: multiple choice, 1: short answer, 2: OX quiz)
+        type = random.randrange(0, 2)
+
+        while retry < QUIZ_GENERATE_RETRY:
+            try:
+                response = self.chain.prob(type, keyword)
+                data = {
+                    "id": f'quiz-{uuid.uuid4()}',
+                    "question_number": question_number,
+                    "type": types[type],
+                    "question": response["text"]["question"],
+                    "options": response["text"]["choices"],
+                    "correct": response["text"]["correct"]
+                }
+                break
+            except KeyError:
+                log("warning", "[generator.py > line 37] retry generating quiz due to KeyError")
+                retry += 1
+                continue
+            except Exception as e:
+                log("warning", f"[generator.py > line 41] An unexpected quiz generate error occurred: {e}")
+                retry += 1
+                continue
+        return data
 
     
 #######################################################################################
