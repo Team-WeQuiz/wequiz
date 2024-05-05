bbf5ecede671d328edb7e1f74242ed8161bbb308
noooey <20203065@kookmin.ac.kr>
2024-05-06 05:17:33 +0900

    [main] :bug: Fix: revert keyword extract, mecab 제거

--- model/app/data/keyword.py
+++ model/app/data/keyword.py
@@ -10,39 +10,35 @@ def extract_keywords(split_doc_list, top_n):
     for doc in split_doc_list:
         text_data.append(doc.page_content)
 
-    # 텍스트 데이터 합치기
-    text_string = ' '.join(text_data)
-
-    # 한글 불용어 읽기
+    # 한글, 영어 불용어 읽기
     ko_stop_words = set([word.strip() for word in ko_stopwords.split('\n')])
-
-    # 영어 불용어 읽기
     en_stop_words = set(en_stopwords.words('english'))
 
-    # 토크나이징 및 불용어 제거
-    tokens = word_tokenize(text_string)
-    cleaned_tokens = []
-    for token in tokens:
-        if token.isalnum():
-            if token.isascii():
-                # 영어 토큰인 경우
-                token = token.lower()
-                if token not in en_stop_words and token not in string.punctuation:
-                    cleaned_tokens.append(token)
-            else:
-                # 한글 토큰인 경우
-                if token not in ko_stop_words:
-                    cleaned_tokens.append(token)
+    cleaned_text_data = []
+    for text in text_data:
+        # 토크나이징 및 불용어 제거
+        tokens = word_tokenize(text)
+        cleaned_tokens = []
+        for token in tokens:
+            if token.isalnum():
+                if token.isascii():
+                    # 영어 토큰인 경우
+                    token = token.lower()
+                    if token not in en_stop_words and token not in string.punctuation:
+                        cleaned_tokens.append(token)
+                else:
+                    # 한글 토큰인 경우
+                    if token not in ko_stop_words:
+                        cleaned_tokens.append(token)
 
-    # TF-IDF 벡터라이저 생성
+        cleaned_text_data.append(' '.join(cleaned_tokens))
+
+
     vectorizer = TfidfVectorizer()
-
-    # 전처리된 토큰 리스트에 TF-IDF 적용
-    tfidf_matrix = vectorizer.fit_transform([' '.join(cleaned_tokens)])
-
-    # 중요한 키워드 추출
+    tfidf_matrix = vectorizer.fit_transform(cleaned_text_data)
     feature_names = vectorizer.get_feature_names_out()
-    tfidf_scores = tfidf_matrix.toarray()[0]
+
+    tfidf_scores = tfidf_matrix.sum(axis=0).A1
     top_scores = tfidf_scores.argsort()[-top_n:][::-1]
     keywords = [feature_names[i] for i in top_scores]
 
