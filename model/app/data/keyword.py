93b744fa58389b1e1072721d6e518251e294c2ba
noooey <20203065@kookmin.ac.kr>
2024-05-06 23:33:25 +0900

    [feat/#325] :zap: Improve: 언어에 따른 토크나이저 사용

--- model/app/data/keyword.py
+++ model/app/data/keyword.py
@@ -1,38 +1,33 @@
 import string
+from langdetect import detect
+from konlpy.tag import Mecab
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords as en_stopwords
 from sklearn.feature_extraction.text import TfidfVectorizer
 from data.korean_stopwords import *
 
 def extract_keywords(split_doc_list, top_n):
-    # 문서 리스트 읽기
     text_data = []
     for doc in split_doc_list:
         text_data.append(doc.page_content)
 
-    # 한글, 영어 불용어 읽기
-    ko_stop_words = set([word.strip() for word in ko_stopwords.split('\n')])
-    en_stop_words = set(en_stopwords.words('english'))
-
     cleaned_text_data = []
     for text in text_data:
-        # 토크나이징 및 불용어 제거
-        tokens = word_tokenize(text)
-        cleaned_tokens = []
-        for token in tokens:
-            if token.isalnum():
-                if token.isascii():
-                    # 영어 토큰인 경우
-                    token = token.lower()
-                    if token not in en_stop_words and token not in string.punctuation:
-                        cleaned_tokens.append(token)
-                else:
-                    # 한글 토큰인 경우
-                    if token not in ko_stop_words:
-                        cleaned_tokens.append(token)
+        language = detect(text)
+        
+        if language == 'ko':
+            # 한글 문서인 경우
+            mecab = Mecab()
+            tokens = mecab.morphs(text)
+            ko_stop_words = set([word.strip() for word in ko_stopwords.split('\n')])
+            cleaned_tokens = [token for token in tokens if token not in ko_stop_words]
+        else:
+            # 영어 또는 기타 언어 문서인 경우
+            tokens = word_tokenize(text)
+            en_stop_words = set(en_stopwords.words('english'))
+            cleaned_tokens = [token.lower() for token in tokens if token.isalnum() and token.lower() not in en_stop_words and token not in string.punctuation]
 
         cleaned_text_data.append(' '.join(cleaned_tokens))
-
 
     vectorizer = TfidfVectorizer()
     tfidf_matrix = vectorizer.fit_transform(cleaned_text_data)
