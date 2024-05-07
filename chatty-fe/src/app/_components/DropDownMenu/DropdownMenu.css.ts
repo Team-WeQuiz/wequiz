c9ebd3f624f09633f7b837e4f280ab71ee7cbe29
Woorim Kim <wrkimlimi@gmail.com>
2024-05-07 14:55:27 +0900

    [feat/#331] :sparkles: Feat: dropdown menu for mypage, logout

---
+++ chatty-fe/src/app/_components/DropDownMenu/DropdownMenu.css.ts
@@ -0,0 +1,12 @@
+import { style } from '@vanilla-extract/css';
+
+export const menuContainer = style({
+  width: '100%',
+});
+
+export const ulList = style({
+  listStyle: 'none',
+  padding: 0,
+  margin: 0,
+  width: '100%',
+});
