a846687b657daa428aa13e07b9a0f4a604693991
ryanbae94 <ryanbae94@gmail.com>
2024-05-07 18:38:59 +0900

    [feat/#324] args 수정

--- chatty-fe/src/app/(game)/quiz-room/[id]/_components/ResultModal/ResultModal.stories.ts
+++ chatty-fe/src/app/(game)/quiz-room/[id]/_components/ResultModal/ResultModal.stories.ts
@@ -13,14 +13,15 @@ export const Default: Story = {
   args: {
     currentRound: 1,
     users: [
-      { id: 1, nickname: '박규연', score: 30 },
-      { id: 2, nickname: '김우림', score: 70 },
-      { id: 3, nickname: '배준형', score: 999 },
-      { id: 4, nickname: '안금장', score: 70 },
-      { id: 6, nickname: '호대용', score: 45 },
-      { id: 7, nickname: '최준숙', score: 20 },
-      { id: 8, nickname: '박하멍', score: 100 },
-      { id: 9, nickname: '쨈민이', score: 10 },
+      { playerId: 1, nickname: '박규연', score: 30 },
+      { playerId: 2, nickname: '김우림', score: 70 },
+      { playerId: 3, nickname: '배준형', score: 999 },
+      { playerId: 4, nickname: '안금장', score: 70 },
+      { playerId: 6, nickname: '호대용', score: 45 },
+      { playerId: 7, nickname: '최준숙', score: 20 },
+      { playerId: 8, nickname: '박하멍', score: 100 },
+      { playerId: 9, nickname: '쨈민이', score: 10 },
     ],
+    count: 10,
   },
 };
