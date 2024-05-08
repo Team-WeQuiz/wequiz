6933919d7364db561c1f8fc2eddd969867ca4c3a
koomchang <leahpar0401@gmail.com>
2024-05-08 15:58:33 +0900

    [feat/#342] :sparkles: Feat: 비밀번호 재설정 api 구현

--- chatty-be/src/main/java/com/chatty/chatty/user/controller/UserController.java
+++ chatty-be/src/main/java/com/chatty/chatty/user/controller/UserController.java
@@ -8,20 +8,15 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RestController("/user")
 @RequiredArgsConstructor
 public class UserController {
 
     private final UserService userService;
 
-
-    @GetMapping("/user")
+    @GetMapping
     public ResponseEntity<UserInfoResponse> getUserInfo(@AuthUser Long userId) {
         return ResponseEntity.ok(userService.getUserInfo(userId));
     }
 
-    @GetMapping("/ping")
-    public ResponseEntity<String> ping() {
-        return ResponseEntity.ok("pong");
-    }
 }
