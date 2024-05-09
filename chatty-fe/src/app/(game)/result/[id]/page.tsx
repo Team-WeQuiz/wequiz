6f282e87ea2b8d1f5e1abe3215b3479e507da641
ryanbae94 <ryanbae94@gmail.com>
2024-05-09 20:29:09 +0900

    [feat/#370] 결과보기 1페이지

--- chatty-fe/src/app/(game)/result/[id]/page.tsx
+++ chatty-fe/src/app/(game)/result/[id]/page.tsx
@@ -1,7 +1,110 @@
-import React from 'react'
+'use client';
+import React, { useEffect, useState } from 'react';
+import * as styles from './page.css';
+import client from '@/app/_api/client';
+import useAuthStore from '@/app/_store/useAuthStore';
+import QuestionProgess from '../../quiz-room/[id]/_components/QuestionProgress/QuestionProgess';
+import useUserInfoStore from '@/app/_store/useUserInfoStore';
+import PercentageCircle from './_components/PercentageCircle/PercentageCircle';
+import Image from 'next/image';
 
-export default function ResultPage() {
+type PlayerAnswers = {
+  playerId: number;
+  nickname: string;
+  playerAnswer: string;
+  marking: boolean;
+};
+
+type Results = {
+  quizNumber: number;
+  quiz: string;
+  options: string[];
+  correct: string;
+  playerAnswers: PlayerAnswers[];
+  correctRate: number;
+};
+
+export default function ResultPage({ params }: { params: { id: number } }) {
+  const [results, setResults] = useState<Results[]>([]);
+  const { accessToken } = useAuthStore();
+  const { id } = useUserInfoStore();
+  const fetchResultData = async () => {
+    try {
+      const result = await client.get(`/rooms/${params.id}/result`, {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      });
+      const data: Results[] = result.data.results;
+      console.log('results: ', data);
+      setResults(data);
+    } catch (error) {
+      console.error('error', error);
+    }
+  };
+
+  useEffect(() => {
+    fetchResultData();
+  }, [accessToken]);
   return (
-    <div>page</div>
-  )
+    <div className={styles.Container}>
+      <div className={styles.SideBarWrapper}>
+        <div className={styles.SideBar}>
+          <QuestionProgess
+            questionNumber={results.length}
+            totalQuestions={results.length}
+            direction="column"
+          />
+        </div>
+      </div>
+      <div className={styles.ContentsContainer}>
+        <div className={styles.ContentsBox}>
+          <div className={styles.ContentsWrapper}>
+            {results.map((result, index) => (
+              <div className={styles.ContentsCard} key={index}>
+                <span className={styles.Number}>{result.quizNumber}</span>
+                <div className={styles.QuestionCard}>
+                  <span className={styles.QuestionMark}>Q</span>
+                  {result.quiz}
+                </div>
+                {result.playerAnswers
+                  .filter((answer) => answer.playerId === id)
+                  .map((answer) => (
+                    <div
+                      key={id}
+                      className={
+                        answer.playerAnswer === result.correct
+                          ? styles.CorrectAnswerCard
+                          : styles.WrongAnswerCard
+                      }
+                    >
+                      <span
+                        className={
+                          answer.playerAnswer === result.correct
+                            ? styles.CorrectAnswerMark
+                            : styles.WrongAnswerMark
+                        }
+                      >
+                        A
+                      </span>
+                      {answer.playerAnswer}
+                    </div>
+                  ))}
+                <PercentageCircle percentage={result.correctRate} />
+                <button className={styles.ModalButton}>
+                  <span>상세보기</span>
+                  <Image
+                    src="/images/arrow_right.svg"
+                    height={48}
+                    width={48}
+                    alt="arrow"
+                  />
+                </button>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
