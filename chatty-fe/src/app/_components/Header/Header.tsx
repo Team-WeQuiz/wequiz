911861e92c0013d3e8cefa402bd99c4e810aae17
Woorim Kim <wrkimlimi@gmail.com>
2024-05-12 02:08:34 +0900

    [feat/#410] :sparkles: Feat: add sound button

--- chatty-fe/src/app/_components/Header/Header.tsx
+++ chatty-fe/src/app/_components/Header/Header.tsx
@@ -3,22 +3,23 @@
 import React, { useState } from 'react';
 import * as styles from './Header.css';
 import Image from 'next/image';
-import { useRouter } from 'next/navigation';
+import { usePathname, useRouter } from 'next/navigation';
 import DropdownMenu from '../DropDownMenu/DropdownMenu';
 import Cookies from 'js-cookie';
 import useAuthStore from '@/app/_store/useAuthStore';
 import useUserInfoStore from '@/app/_store/useUserInfoStore';
 
 export default function Header() {
+  const pathName = usePathname();
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   const navigate = useRouter();
   const { deleteTokens } = useAuthStore();
   const { deleteUserInfo } = useUserInfoStore();
+  const [isMusicOn, setIsMusicOn] = useState(true);
 
   const handleLogoClick = () => {
     navigate.push('/main-lobby');
   };
-
   const handleProfileClick = (
     e: React.MouseEvent<HTMLButtonElement, MouseEvent>,
   ) => {
@@ -32,42 +33,63 @@ export default function Header() {
     Cookies.remove('refreshToken');
     navigate.push('/');
   };
+
+  const handleMusicPlay = () => {
+    setIsMusicOn(!isMusicOn);
+  };
+
   return (
     <header className={styles.container}>
       <button onClick={handleLogoClick} className={styles.mainButton}>
         <Image src="/images/logo.svg" height={52} width={112} alt="logo" />
       </button>
-      <div>
-        <div className={styles.profileButton}>
-          <button
-            onClick={(e) => handleProfileClick(e)}
-            className={styles.mainButton}
-          >
-            <Image
-              src="/images/person.svg"
-              height={48}
-              width={48}
-              alt="profile"
-            />
-          </button>
-          <div className={styles.dropdownMenu}>
-            <DropdownMenu isOpen={isMenuOpen} setIsOpen={setIsMenuOpen}>
-              <li className={styles.dropdownMenuList}>
-                <button
-                  onClick={() => navigate.push('/mypage')}
-                  className={styles.menuButton}
-                >
-                  마이페이지
-                </button>
-              </li>
-              <li className={styles.dropdownMenuList}>
-                <button onClick={handleLogout} className={styles.menuButton}>
-                  로그아웃
-                </button>
-              </li>
-            </DropdownMenu>
-          </div>
-        </div>
+      <div className={styles.buttonsWrapper}>
+        <button className={styles.musicPlayButton} onClick={handleMusicPlay}>
+          <Image
+            src={
+              isMusicOn ? '/images/volume_max.svg' : '/images/volume_mute.svg'
+            }
+            height={36}
+            width={36}
+            alt={isMusicOn ? 'music_on' : 'music_off'}
+          />
+        </button>
+        {!pathName.includes('/waiting-room') &&
+          !pathName.includes('/quiz-room') && (
+            <div className={styles.profileButton}>
+              <button
+                onClick={(e) => handleProfileClick(e)}
+                className={styles.mainButton}
+              >
+                <Image
+                  src="/images/person.svg"
+                  height={48}
+                  width={48}
+                  alt="profile"
+                />
+              </button>
+              <div className={styles.dropdownMenu}>
+                <DropdownMenu isOpen={isMenuOpen} setIsOpen={setIsMenuOpen}>
+                  <li className={styles.dropdownMenuList}>
+                    <button
+                      onClick={() => navigate.push('/mypage')}
+                      className={styles.menuButton}
+                    >
+                      마이페이지
+                    </button>
+                  </li>
+                  <li className={styles.dropdownMenuList}>
+                    <button
+                      onClick={handleLogout}
+                      className={styles.menuButton}
+                    >
+                      로그아웃
+                    </button>
+                  </li>
+                </DropdownMenu>
+              </div>
+            </div>
+          )}
       </div>
     </header>
   );
