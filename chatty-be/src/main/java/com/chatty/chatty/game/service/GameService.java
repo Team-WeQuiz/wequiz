1901dc954dda1820a203b0ca08599b05954be03f
koomchang <leahpar0401@gmail.com>
2024-05-13 13:41:14 +0900

    [feat/#441] :bug: Fix: 퀴즈룸 join이 호출 될 때 저장된 정보가 있으면 다시 가져오도록 수정

--- chatty-be/src/main/java/com/chatty/chatty/game/service/GameService.java
+++ chatty-be/src/main/java/com/chatty/chatty/game/service/GameService.java
@@ -71,8 +71,8 @@ public class GameService {
 
     public PlayersStatusDTO joinRoom(Long roomId, Long userId, NicknameRequest request) {
         String profileImageUrl = userService.getProfileImageUrl(userId);
-        PlayersStatus playersStatus = playersStatusRepository.saveUserToRoom(roomId, userId, request.nickname(),
-                profileImageUrl);
+        PlayersStatus playersStatus = playersStatusRepository.findByRoomId(roomId)
+                .orElse(playersStatusRepository.saveUserToRoom(roomId, userId, request.nickname(), profileImageUrl));
         return buildDTO(roomId, playersStatus);
     }
 
@@ -85,11 +85,7 @@ public class GameService {
         PlayersStatus playersStatus = playersStatusRepository.toggleReady(roomId, userId);
         if (playersStatus.isAllReady()) {
             quizRoomRepository.updateStatusById(roomId, Status.STARTED);
-            log.info("모두 레디 상태");
-            log.info("room status: {}", quizRoomRepository.findById(roomId).get().getStatus());
         } else {
-            log.info("레디 상태 아님");
-            log.info("room status: {}", quizRoomRepository.findById(roomId).get().getStatus());
             quizRoomRepository.updateStatusById(roomId, Status.READY);
         }
         return buildDTO(roomId, playersStatus);
