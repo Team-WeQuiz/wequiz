6887f628f6e8af6e770a5d86eb8e4139c27d8455
Woorim Kim <wrkimlimi@gmail.com>
2024-05-28 21:06:54 +0900

    [feat/#508] :sparkles: Feat: add Footer in mypage

---
+++ chatty-fe/src/app/(game)/mypage/_component/Footer/Footer.tsx
@@ -0,0 +1,83 @@
+import * as styles from './Footer.css';
+import Image from 'next/image';
+import Link from 'next/link';
+
+const Footer = () => {
+  return (
+    <footer className={styles.mypageFooter}>
+      <div className={styles.footerWrapper}>
+        <Image
+          src="/images/footer-logo.svg"
+          alt="logo"
+          width={200}
+          height={100}
+          className={styles.footerLogo}
+        />
+        <div className={styles.footerLinks}>
+          <Link
+            href="https://team-wequiz.github.io/wequiz/"
+            target="_blank"
+            className={styles.link}
+          >
+            <span className={styles.linkText}>About Us</span>
+          </Link>
+          <Link
+            href="https://설문 링크"
+            target="_blank"
+            className={styles.link}
+          >
+            <span className={styles.linkText}>Send Feedback</span>
+          </Link>
+          <div>
+            <span
+              className={styles.linkText}
+              style={{ textDecoration: 'none' }}
+            >
+              Contact Us
+            </span>
+            <div className={styles.linkIcons}>
+              <Link
+                href="https://github.com/Team-WeQuiz/wequiz"
+                target="_blank"
+              >
+                <Image
+                  src="/images/github-icon.svg"
+                  alt="github"
+                  width={24}
+                  height={24}
+                  className={styles.socialIcon}
+                />
+              </Link>
+              <Link href="mailto:wequiz.contact@gmail.com" target="_blank">
+                <Image
+                  src="/images/google-icon.svg"
+                  alt="mail"
+                  width={24}
+                  height={24}
+                  className={styles.socialIcon}
+                />
+              </Link>
+              <Link
+                href="https://www.instagram.com/wequiz_official/"
+                target="_blank"
+              >
+                <Image
+                  src="/images/instagram-icon.svg"
+                  alt="instagram"
+                  width={24}
+                  height={24}
+                  className={styles.socialIcon}
+                />
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+      <span className={styles.copyRight}>
+        © Copyright 2024. WeQuiz All rights reserved.
+      </span>
+    </footer>
+  );
+};
+
+export default Footer;
--- chatty-fe/src/app/(game)/mypage/page.tsx
+++ chatty-fe/src/app/(game)/mypage/page.tsx
@@ -4,6 +4,7 @@ import { useState } from 'react';
 import * as styles from './page.css';
 import UserInfoForm from './_component/UserInfoForm/UserInfoForm';
 import UserQuizList from './_component/UserQuizList/UserQuizList';
+import Footer from './_component/Footer/Footer';
 
 const MyPage = () => {
   const [tabIndex, setTabIndex] = useState<number>(0);
@@ -23,12 +24,6 @@ const MyPage = () => {
         >
           나의 퀴즈
         </h1>
-        <h1
-          className={`${styles.tabText} ${tabIndex === 2 && styles.tabTextActive}`}
-          onClick={() => setTabIndex(2)}
-        >
-          문의
-        </h1>
       </div>
       <div
         className={`${styles.contentContainer} ${tabIndex !== 0 && styles.mobileHidden}`}
@@ -46,12 +41,7 @@ const MyPage = () => {
           <UserQuizList />
         </div>
       </div>
-      <div
-        className={`${styles.contentContainer} ${tabIndex !== 2 && styles.mobileHidden}`}
-      >
-        <h1 className={styles.headerText}>문의</h1>
-        <div className={styles.contentBox}></div>
-      </div>
+      <Footer />
     </div>
   );
 };
